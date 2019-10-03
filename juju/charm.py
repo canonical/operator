33087e318b3e393e17fc22a078e0ec7b5775dc6a
Cory Johns <johnsca@gmail.com>
2019-10-03 10:38:15 -0400

    First pass at dynamic events (relations, storage) (#8)

--- juju/charm.py
+++ juju/charm.py
@@ -13,6 +13,40 @@ class LeaderElectedEvent(EventBase): pass
 class LeaderSettingsChangedEvent(EventBase): pass
 
 
+class RelationEventBase(EventBase):
+    def __init__(self, handle, relation_name):
+        super().__init__(handle)
+        self.relation_name = relation_name
+
+    def snapshot(self):
+        return {'relation_name': self.relation_name}
+
+    def restore(self, snapshot):
+        self.relation_name = snapshot['relation_name']
+
+
+class RelationJoinedEvent(RelationEventBase): pass
+class RelationChangedEvent(RelationEventBase): pass
+class RelationDepartedEvent(RelationEventBase): pass
+class RelationBrokenEvent(RelationEventBase): pass
+
+
+class StorageEventBase(EventBase):
+    def __init__(self, handle, storage_name):
+        super().__init__(handle)
+        self.storage_name = storage_name
+
+    def snapshot(self):
+        return {'storage_name': self.storage_name}
+
+    def restore(self, snapshot):
+        self.storage_name = snapshot['storage_name']
+
+
+class StorageAttachedEvent(StorageEventBase): pass
+class StorageDetachingEvent(StorageEventBase): pass
+
+
 class CharmEvents(EventsBase):
 
     install = Event(InstallEvent)
@@ -30,3 +64,24 @@ class CharmEvents(EventsBase):
 class CharmBase(Object):
 
     on = CharmEvents()
+
+    def __init__(self, framework, key, metadata):
+        super().__init__(framework, key)
+        self.metadata = metadata
+
+        for role in ('requires', 'provides', 'peers'):
+            for relation_name in metadata.get(role, ()):
+                self.on.define_event(f'{relation_name}_relation_joined',
+                                     RelationJoinedEvent)
+                self.on.define_event(f'{relation_name}_relation_changed',
+                                     RelationChangedEvent)
+                self.on.define_event(f'{relation_name}_relation_departed',
+                                     RelationDepartedEvent)
+                self.on.define_event(f'{relation_name}_relation_broken',
+                                     RelationBrokenEvent)
+
+        for storage_name in metadata.get('storage', ()):
+            self.on.define_event(f'{storage_name}_storage_attached',
+                                 StorageAttachedEvent)
+            self.on.define_event(f'{storage_name}_storage_detaching',
+                                 StorageDetachingEvent)
--- test/test_charm.py
+++ test/test_charm.py
@@ -25,9 +25,9 @@ class TestCharm(unittest.TestCase):
 
         class MyCharm(CharmBase):
 
-            def __init__(self, framework, key):
-                super().__init__(framework, key)
-                
+            def __init__(self, framework, key, metadata):
+                super().__init__(framework, key, metadata)
+
                 self.started = False
                 framework.observe(self.on.start, self)
 
@@ -35,10 +35,91 @@ class TestCharm(unittest.TestCase):
                 self.started = True
 
         framework = self.create_framework()
-        charm = MyCharm(framework, None)
+        charm = MyCharm(framework, None, {})
         charm.on.start.emit()
 
         self.assertEqual(charm.started, True)
+
+    def test_relation_events(self):
+
+        class MyCharm(CharmBase):
+            def __init__(self, framework, key, metadata):
+                super().__init__(framework, key, metadata)
+                self.seen = []
+                for event_kind, bound_event in self.on.events():
+                    # hook up relation events to generic handler
+                    if 'relation' in event_kind:
+                        framework.observe(bound_event, self.on_any_relation)
+
+            def on_any_relation(self, event):
+                self.seen.append(f'{type(event).__name__} on '
+                                 f'{event.relation_name}')
+
+        metadata = {
+            'name': 'my-charm',
+            'requires': {
+                'req1': {'interface': 'req1'},
+                'req2': {'interface': 'req2'},
+            },
+            'provides': {
+                'pro1': {'interface': 'pro1'},
+                'pro2': {'interface': 'pro2'},
+            },
+            'peers': {
+                'peer1': {'interface': 'peer1'},
+            },
+        }
+
+        charm = MyCharm(self.create_framework(), None, metadata)
+
+        charm.on.req1_relation_joined.emit('req1')
+        charm.on.req1_relation_changed.emit('req1')
+        charm.on.req2_relation_changed.emit('req2')
+        charm.on.pro1_relation_departed.emit('pro1')
+        charm.on.peer1_relation_broken.emit('peer1')
+
+        self.assertEqual(charm.seen, [
+            'RelationJoinedEvent on req1',
+            'RelationChangedEvent on req1',
+            'RelationChangedEvent on req2',
+            'RelationDepartedEvent on pro1',
+            'RelationBrokenEvent on peer1',
+        ])
+
+    def test_storage_events(self):
+
+        class MyCharm(CharmBase):
+            def __init__(self, framework, key, metadata):
+                super().__init__(framework, key, metadata)
+                self.seen = []
+                framework.observe(self.on.stor1_storage_attached, self)
+                framework.observe(self.on.stor2_storage_detaching, self)
+
+            def on_stor1_storage_attached(self, event):
+                self.seen.append(f'{type(event).__name__} on '
+                                 f'{event.storage_name}')
+
+            def on_stor2_storage_detaching(self, event):
+                self.seen.append(f'{type(event).__name__} on '
+                                 f'{event.storage_name}')
+
+        metadata = {
+            'name': 'my-charm',
+            'storage': {
+                'stor1': {'type': 'filesystem'},
+                'stor2': {'type': 'filesystem'},
+            },
+        }
+
+        charm = MyCharm(self.create_framework(), None, metadata)
+
+        charm.on.stor1_storage_attached.emit('stor1')
+        charm.on.stor2_storage_detaching.emit('stor2')
+
+        self.assertEqual(charm.seen, [
+            'StorageAttachedEvent on stor1',
+            'StorageDetachingEvent on stor2',
+        ])
 
 
 if __name__ == "__main__":
