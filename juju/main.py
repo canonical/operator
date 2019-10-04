c2ee066cd8d69670b69d0b4120006b52cbca2055
Dmitrii Shcherbakov <dshcherb@users.noreply.github.com>
2019-10-04 19:29:44 +0300

    Add entrypoint code (#6)
    Adds entrypoint code which will be used by charm implementations in order to initialize the framework and other in memory state and dispatch a new Juju event.

---
+++ juju/main.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import yaml
+from pathlib import Path
+
+CHARM_STATE_FILE = '.unit-state.db'
+
+
+def debugf(format, *args, **kwargs):
+    pass
+
+
+def _get_charm_dir():
+    charm_dir = os.environ.get("JUJU_CHARM_DIR")
+    if charm_dir is None:
+        # Assume $JUJU_CHARM_DIR/lib/juju/main.py structure.
+        charm_dir = Path(f'{__file__}/../../..').resolve()
+    else:
+        charm_dir = Path(charm_dir).resolve()
+    return charm_dir
+
+
+def _load_metadata():
+    with open('metadata.yaml') as f:
+        metadata = yaml.load(f, Loader=yaml.SafeLoader)
+    return metadata
+
+
+def _handle_event_link(charm_dir, event_name):
+    """Create a symlink for a particular event.
+
+    charm_dir -- A root directory of the charm
+    event_name -- A name of the event for which to create a symlink.
+    """
+    event_hook_path = charm_dir / 'hooks' / event_name.replace('_', '-')
+    create_link = True
+    # Remove incorrect symlinks or files.
+    if event_hook_path.exists():
+        # Non-symlink entries and the ones not pointing to "install"
+        # need to be removed.
+        if not event_hook_path.is_symlink():
+            debugf(f'Hook entry at {event_hook_path} is not a symlink:'
+                   ' attempting to remove it.')
+            # May raise IsADirectoryError, e.g. in case it is a directory which
+            # is unexpected and left to the developer or operator to handle.
+            event_hook_path.unlink()
+        elif os.readlink(event_hook_path) != 'install':
+            debugf(f'Removing entry {event_hook_path} as it does not point'
+                   ' to "install"')
+            event_hook_path.unlink()
+        else:
+            create_link = False
+
+    if create_link:
+        debugf(f'Creating a new relative symlink at {event_hook_path}'
+               f' to pointing to "install" located in {charm_dir}/hooks')
+        event_hook_path.symlink_to('install')
+
+
+def _setup_hooks(charm_dir, charm):
+    """Set up hooks for supported events.
+
+    Whether a charm can handle an event or not can be determined by
+    introspecting which events are defined on it.
+
+    Hooks are created as symlinks to "install" which may be either
+    a copy of main.py (which calls the main function) or a symlink
+    to it. Note that it is important to avoid creating a recursive
+    symlink which is why the install event itself is skipped.
+
+    charm_dir -- A root directory of the charm.
+    charm -- An instance of the Charm class.
+    """
+    for event_name in charm.on.events().keys():
+        if event_name != 'install':
+            _handle_event_link(charm_dir, event_name)
+
+
+def _emit_charm_event(charm, event_name):
+    """Emits a charm event based on a Juju event name.
+
+    charm -- A charm instance to emit an event from.
+    event_name -- A Juju event name to emit on a charm.
+    """
+    formatted_event_name = event_name.replace('-', '_')
+    event_to_emit = None
+    try:
+        event_to_emit = getattr(charm.on, formatted_event_name)
+    except AttributeError:
+        debugf(f"event {formatted_event_name} not defined for {charm}")
+
+    # If the event is not supported by the charm implementation, do
+    # not error out or try to emit it. This is to support rollbacks.
+    if event_to_emit is not None:
+        debugf(f'Emitting Juju event {event_name}')
+        event_to_emit.emit()
+
+
+def main():
+    """Setup the charm and dispatch the observed event.
+
+    The event name is based on the way this executable was called (argv[0]).
+    """
+    import charm as charm_module
+    import juju.charm
+    import juju.framework
+
+    charm_dir = _get_charm_dir()
+
+    # Process the Juju event relevant to the current hook execution
+    # JUJU_HOOK_NAME or JUJU_ACTION_NAME are not used to support simulation
+    # of events from debugging sessions.
+    juju_event_name = Path(sys.argv[0]).name
+
+    # TODO: If Juju unit agent crashes after exit(0) from the charm code
+    # the framework will commit the snapshot but Juju will not commit its
+    # operation.
+    charm_state_path = charm_dir / CHARM_STATE_FILE
+    framework = juju.framework.Framework(data_path=charm_state_path)
+    try:
+        charm = charm_module.Charm(framework, None, _load_metadata())
+
+        # When a charm is force-upgraded and a unit is in an error state Juju does not run upgrade-charm and
+        # instead runs the failed hook followed by config-changed. Given the nature of force-upgrading
+        # the hook setup code is not triggered on config-changed.
+        if (juju_event_name in ('install', 'upgrade-charm') or juju_event_name.endswith('-storage-attached')):
+            _setup_hooks(charm_dir, charm)
+
+        framework.reemit()
+
+        _emit_charm_event(charm, juju_event_name)
+
+        framework.commit()
+    finally:
+        framework.close()
+
+
+def _setup_path():
+    # The first element is the directory containing this file. We don't want that in the path.
+    del sys.path[0]
+    # Add $JUJU_CHARM_DIR/lib to the path.
+    sys.path.insert(0, str(_get_charm_dir() / 'lib'))
+
+
+if __name__ == '__main__':
+    _setup_path()
+    main()
---
+++ test/test_main.py
@@ -0,0 +1,240 @@
+#!/usr/bin/env python3
+
+import unittest
+import logging
+import os
+import sys
+import subprocess
+import pickle
+import base64
+import tempfile
+
+import importlib.util
+
+import juju.main
+
+from pathlib import Path
+
+from juju.charm import (
+    CharmBase,
+    CharmEvents,
+    InstallEvent,
+    ConfigChangedEvent,
+    UpdateStatusEvent,
+    LeaderSettingsChangedEvent,
+    RelationJoinedEvent,
+    RelationChangedEvent,
+    RelationBrokenEvent,
+)
+
+# This relies on the expected repository structure to find
+# a path to the charm under test.
+JUJU_CHARM_DIR = Path(f'{__file__}/../charms/test_main').resolve()
+
+charm_spec = importlib.util.spec_from_file_location("charm", str(JUJU_CHARM_DIR / 'lib/charm.py'))
+charm = importlib.util.module_from_spec(charm_spec)
+charm_spec.loader.exec_module(charm)
+
+
+logger = logging.getLogger(__name__)
+
+
+class SymlinkTargetError(Exception):
+    pass
+
+
+class TestMain(unittest.TestCase):
+
+    MAIN_PY_RELPATH = '../lib/juju/main.py'
+
+    @classmethod
+    def _clear_unit_db(cls):
+        charm_state_file = JUJU_CHARM_DIR / juju.main.CHARM_STATE_FILE
+        if charm_state_file.exists():
+            charm_state_file.unlink()
+
+    def setUp(self):
+        self._clear_unit_db()
+        self._clear_symlinks()
+
+        # Change cwd for the current process to the test charm directory
+        # as it is preserved across fork + exec.
+        self.addCleanup(os.chdir, os.curdir)
+        os.chdir(JUJU_CHARM_DIR)
+        _, tmp_file = tempfile.mkstemp()
+        self._state_file = Path(tmp_file)
+
+        # Relations events are defined dynamically and modify the class attributes.
+        # We use a subclass temporarily to prevent these side effects from leaking.
+        class TestCharmEvents(CharmEvents):
+            pass
+        CharmBase.on = TestCharmEvents()
+
+    def tearDown(self):
+        self._clear_unit_db()
+        self._clear_symlinks()
+
+        self._state_file.unlink()
+
+        CharmBase.on = CharmEvents()
+
+    @classmethod
+    def _clear_symlinks(cls):
+        r, _, files = next(os.walk(JUJU_CHARM_DIR / 'hooks'))
+        for f in files:
+            absolute_path = Path(r) / f
+            if absolute_path.name == 'install' and absolute_path.is_symlink():
+                if os.readlink(absolute_path) != cls.MAIN_PY_RELPATH:
+                    raise SymlinkTargetError(f'"{absolute_path.name}" link does not point to {cls.MAIN_PY_RELPATH}')
+            elif absolute_path.name.endswith('-storage-attached') and absolute_path.is_symlink():
+                if os.readlink(absolute_path) != 'install':
+                    raise SymlinkTargetError(f'"{absolute_path.name}" link does not point to "install"')
+            else:
+                absolute_path.unlink()
+
+    def _read_and_clear_state(self):
+        state = None
+        if self._state_file.stat().st_size:
+            with open(self._state_file, 'r+b') as state_file:
+                state = pickle.load(state_file)
+                state_file.truncate()
+        return state
+
+    def _simulate_event(self, event_name, charm_config):
+        event_hook = JUJU_CHARM_DIR / f"hooks/{event_name.replace('_', '-')}"
+        # Note that sys.executable is used to make sure we are using the same
+        # interpreter for the child process to support virtual environments.
+        subprocess.check_call(
+            [sys.executable, event_hook],
+            env={
+                'JUJU_CHARM_DIR': JUJU_CHARM_DIR,
+                'CHARM_CONFIG': charm_config,
+            })
+        return self._read_and_clear_state()
+
+    def test_event_reemitted(self):
+        # base64 encoding is used to avoid null bytes.
+        charm_config = base64.b64encode(pickle.dumps({
+            'STATE_FILE': self._state_file,
+        }))
+
+        # First run "install" to make sure all hooks are set up.
+        state = self._simulate_event('install', charm_config)
+        self.assertEqual(state['observed_event_types'], [InstallEvent])
+
+        state = self._simulate_event('config-changed', charm_config)
+        self.assertEqual(state['observed_event_types'], [ConfigChangedEvent])
+
+        # Re-emit should pick the deferred config-changed.
+        state = self._simulate_event('update-status', charm_config)
+        self.assertEqual(state['observed_event_types'], [ConfigChangedEvent, UpdateStatusEvent])
+
+    def test_multiple_events_handled(self):
+
+        # Sample events with a different amount of dashes used
+        # and with endpoints from different sections of metadata.yaml
+        events_under_test = {
+            'install': InstallEvent,
+            'update_status': UpdateStatusEvent,
+            'leader_settings_changed': LeaderSettingsChangedEvent,
+            'db_relation_joined': RelationJoinedEvent,
+            'mon_relation_changed': RelationChangedEvent,
+            'ha_relation_broken': RelationBrokenEvent,
+        }
+
+        logger.debug(f'Expected events {events_under_test}')
+
+        charm_config = base64.b64encode(pickle.dumps({
+            'STATE_FILE': self._state_file,
+        }))
+
+        # First run "install" to make sure all hooks are set up.
+        self._simulate_event('install', charm_config)
+
+        # Simulate hook executions for every event.
+        for event_kind, event in events_under_test.items():
+            state = self._simulate_event(event_kind, charm_config)
+
+            handled_events = state.get(f'on_{event_kind}', [])
+            # Make sure that a handler for that event was called once.
+            self.assertEqual(len(handled_events), 1)
+            # Make sure the event handled by the Charm has the right type.
+            handled_event_type = handled_events[0]
+            self.assertEqual(handled_event_type, event)
+
+            self.assertEqual(state['observed_event_types'], [event])
+
+    def test_event_not_implemented(self):
+        """Make sure events without implementation do not cause non-zero exit.
+        """
+        charm_config = base64.b64encode(pickle.dumps({
+            'STATE_FILE': self._state_file,
+        }))
+
+        # Simulate a scenario where there is a symlink for an event that
+        # a charm does not know how to handle.
+        hook_path = JUJU_CHARM_DIR / 'hooks/not-implemented-event'
+        # This will be cleared up in tearDown.
+        hook_path.symlink_to('install')
+
+        try:
+            self._simulate_event('not-implemented-event', charm_config)
+        except subprocess.CalledProcessError:
+            self.fail('Event simulation for an unsupported event'
+                      ' results in a non-zero exit code returned')
+
+    def test_setup_hooks(self):
+        """Test auto-creation of symlinks for supported events.
+        """
+        event_hooks = [f'hooks/{e.replace("_", "-")}'
+                       for e in charm.Charm.on.events().keys()
+                       if e != 'install']
+
+        install_link_path = JUJU_CHARM_DIR / 'hooks/install'
+
+        # The symlink is expected to be present in the source tree.
+        self.assertTrue(install_link_path.exists())
+        # It has to point to main.py in the lib directory of the charm.
+        self.assertEqual(os.readlink(install_link_path), self.MAIN_PY_RELPATH)
+
+        def _assess_setup_hooks(event_name):
+            event_hook = JUJU_CHARM_DIR / f'hooks/{event_name}'
+
+            charm_config = base64.b64encode(pickle.dumps({
+                'STATE_FILE': self._state_file,
+            }))
+
+            # Simulate a fork + exec of a hook from a unit agent.
+            self._simulate_event(event_name, charm_config)
+
+            r, _, files = next(os.walk(JUJU_CHARM_DIR / 'hooks'))
+
+            self.assertTrue(event_name in files)
+
+            for event_hook in event_hooks:
+                print(f'event_hook: {event_hook}')
+                self.assertTrue(os.path.exists(event_hook))
+                self.assertEqual(os.readlink(event_hook), 'install')
+                self.assertEqual(os.readlink('hooks/install'),
+                                 self.MAIN_PY_RELPATH)
+
+        # Assess 'install' first because upgrade-charm or other
+        # events cannot be handled before install creates symlinks for them.
+        events_to_assess = ['install', 'start', 'config-changed',
+                            'leader-elected', 'upgrade-charm', 'update-status']
+
+        for event_name in events_to_assess:
+            _assess_setup_hooks(event_name)
+
+        self._clear_symlinks()
+
+        # Storage hooks run before "install" so this case needs to be checked as well.
+        events_to_assess = ['disks-storage-attached', 'install', 'start', 'config-changed',
+                            'leader-elected', 'upgrade-charm', 'update-status']
+
+        for event_name in events_to_assess:
+            _assess_setup_hooks(event_name)
+
+
+if __name__ == "__main__":
+    unittest.main()
