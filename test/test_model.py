d9de45350286ce9bc36ee6886d02bae11b96b47a
Cory Johns <johnsca@gmail.com>
2019-10-16 16:48:22 -0400

    Split up large relation test

--- test/test_model.py
+++ test/test_model.py
@@ -53,42 +53,55 @@ class TestModel(unittest.TestCase):
     def setUp(self):
         self.model = juju.model.Model('myapp/0', ['db0', 'db1', 'db2'], TestModelBackend())
 
-    def test_relations(self):
+    def test_model(self):
         self.assertIs(self.model.app, self.model.unit.app)
+
+    def test_relations_keys(self):
         for relation in self.model.relations['db2']:
             self.assertIn(self.model.unit, relation.data)
             unit_from_rel = next(filter(lambda u: u.name == 'myapp/0', relation.data.keys()))
             self.assertIs(self.model.unit, unit_from_rel)
+
+    def test_relation_helper(self):
         self.assertIsNone(self.model.relation('db0'))
         self.assertIsInstance(self.model.relation('db1'), juju.model.Relation)
         with self.assertRaises(juju.model.TooManyRelatedApps):
             self.model.relation('db2')
+
+    def test_relation_data(self):
         random_unit = self.model._cache.get(juju.model.Unit, 'randomunit/0')
         with self.assertRaises(KeyError):
             self.model.relation('db1').data[random_unit]
         remoteapp1_0 = next(filter(lambda u: u.name == 'remoteapp1/0', self.model.relation('db1').units))
         self.assertEqual(self.model.relation('db1').data[remoteapp1_0], {'host': 'remoteapp1-0'})
+
+    def test_relation_data_modify_remote(self):
         rel_db1 = self.model.relation('db1')
         backend = self.model._backend
-        # Verify that we can't modify relation data for other units.
+        remoteapp1_0 = next(filter(lambda u: u.name == 'remoteapp1/0', self.model.relation('db1').units))
         with self.assertRaises(TypeError):
             rel_db1.data[remoteapp1_0]['foo'] = 'bar'
         self.assertFalse(backend.relation_set_called)
-        # Force the relation data for the local unit to be read into memory. We have to do this because
-        # our fake relation_get doesn't honor values previously set by relation_set like the real Juju
-        # hook commands would.
-        self.assertNotIn('foo', rel_db1.data[self.model.unit])
-        # Verify that we can modify our own relation data.
-        rel_db1.data[self.model.unit]['foo'] = 'bar'
+
+    def test_relation_data_modify_local(self):
+        rel_db1 = self.model.relation('db1')
+        backend = self.model._backend
+        self.assertIn('host', rel_db1.data[self.model.unit])
+        rel_db1.data[self.model.unit]['host'] = 'bar'
         self.assertTrue(backend.relation_set_called)
-        self.assertEqual(rel_db1.data[self.model.unit]['foo'], 'bar')
-        backend.relation_set_called = False
-        # Verify that we can delete relation keys.
-        del rel_db1.data[self.model.unit]['foo']
+        self.assertEqual(rel_db1.data[self.model.unit]['host'], 'bar')
+
+    def test_relation_data_del_key(self):
+        rel_db1 = self.model.relation('db1')
+        backend = self.model._backend
+        self.assertIn('host', rel_db1.data[self.model.unit])
+        del rel_db1.data[self.model.unit]['host']
         self.assertTrue(backend.relation_set_called)
-        self.assertNotIn('foo', rel_db1.data[self.model.unit])
-        backend.relation_set_called = False
-        # Verify that relation data values are type-checked as strings.
+        self.assertNotIn('host', rel_db1.data[self.model.unit])
+
+    def test_relation_data_type_check(self):
+        rel_db1 = self.model.relation('db1')
+        backend = self.model._backend
         with self.assertRaises(TypeError):
             rel_db1.data[self.model.unit]['foo'] = 1
         with self.assertRaises(TypeError):
