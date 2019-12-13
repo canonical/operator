762af45ad4afade5d68f402f2ba9450d5aff793d
Cory Johns <johnsca@gmail.com>
2019-12-13 15:44:18 -0500

    Switch to setup.py to avoid flit_core dependency

--- ops/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-Operators Framework for Charms
-"""
-__version__ = '0.0.1'
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+
+setup(
+    name="ops",
+    version="0.0.1",
+    url="https://github.com/canonical/operator",
+    packages=['ops'],
+)
