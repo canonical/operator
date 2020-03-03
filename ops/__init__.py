bf4ba3ffe13563d8e6c873d9944ba70c9cc34316
John R. Lenton <jlenton@gmail.com>
2020-03-03 12:30:50 +0100

    ops: also have copyright headers

--- ops/__init__.py
+++ ops/__init__.py
@@ -1,3 +1,17 @@
+# Copyright 2019 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # note because the first import of ops can be from any of the
 # namespace-providing packages, this __init__.py can't have any code
 # beyond the extend_path call.
