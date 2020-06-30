3470580b6266d18c3cf312c950a940aa0d005518
John R. Lenton <jlenton@gmail.com>
2020-06-30 22:48:48 +0100

    post-release fallback version bump

--- ops/version.py
+++ ops/version.py
@@ -17,7 +17,7 @@ from pathlib import Path
 
 __all__ = ('version',)
 
-_FALLBACK = '0.7'  # this gets bumped after release
+_FALLBACK = '0.8'  # this gets bumped after release
 
 
 def _get_version():
