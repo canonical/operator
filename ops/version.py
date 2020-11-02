78bcfb7188d23ba7aee1cb172cb8219510efcb6b
chipaca <chipaca@users.noreply.github.com>
2020-11-02 20:19:51 +0000

    1.0 fallback version bump (#442)

--- ops/version.py
+++ ops/version.py
@@ -22,7 +22,7 @@ from pathlib import Path
 
 __all__ = ('version',)
 
-_FALLBACK = '0.8'  # this gets bumped after release
+_FALLBACK = '1.0'  # this gets bumped after release
 
 
 def _get_version():
