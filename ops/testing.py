963d75eddaefda5c34656d73b98aea1255df1fe9
Balbir Thomas <balbir.thomas@canonical.com>
2020-11-23 16:45:16 +0000

    Added remove relation and unit methods to harness
    This commit adds method to remove a relation and a
    relation unit to the test harness.

--- ops/testing.py
+++ ops/testing.py
@@ -407,16 +407,45 @@ class Harness:
         self._emit_relation_created(relation_name, rel_id, remote_app)
         return rel_id
 
+    def remove_relation(self, relation_name: str, remote_app: str) -> None:
+        """Remove a relation.
+
+        Args:
+            relation_name: The relation on Charm that is being related to
+            remote_app: The name of the application that is being related to
+
+        Raises:
+            ValueError: if relation_name is invalid
+            RelationNotFoundError: if valid relation id for relation_name is not found
+        """
+        rel_id = self._backend._relation_ids_map[relation_name][0]
+        for unit_name in self._backend._relation_list_map[rel_id]:
+            self.remove_relation_unit(rel_id, unit_name)
+        self._emit_relation_broken(relation_name, rel_id, remote_app)
+        self._backend._relation_app_and_units.pop(rel_id)
+        self._backend._relation_data.pop(rel_id)
+        self._backend._relation_list_map.pop(rel_id)
+        self._backend._relation_ids_map.pop(relation_name)
+        self._backend._relation_names.pop(rel_id)
+
     def _emit_relation_created(self, relation_name: str, relation_id: int,
                                remote_app: str) -> None:
         """Trigger relation-created for a given relation with a given remote application."""
         if self._charm is None or not self._hooks_enabled:
             return
+        relation = self._model.get_relation(relation_name, relation_id)
+        app = self._model.get_app(remote_app)
+        self._charm.on[relation_name].relation_created.emit(
+            relation, app)
+
+    def _emit_relation_broken(self, relation_name: str, relation_id: int,
+                              remote_app: str) -> None:
+        """Trigger relation-broken for a given relation with a given remote application."""
         if self._charm is None or not self._hooks_enabled:
             return
         relation = self._model.get_relation(relation_name, relation_id)
         app = self._model.get_app(remote_app)
-        self._charm.on[relation_name].relation_created.emit(
+        self._charm.on[relation_name].relation_broken.emit(
             relation, app)
 
     def add_relation_unit(self, relation_id: int, remote_unit_name: str) -> None:
@@ -448,17 +477,73 @@ class Harness:
         relation_name = self._backend._relation_names[relation_id]
         # Make sure that the Model reloads the relation_list for this relation_id, as well as
         # reloading the relation data for this unit.
-        if self._model is not None:
-            remote_unit = self._model.get_unit(remote_unit_name)
-            relation = self._model.get_relation(relation_name, relation_id)
-            unit_cache = relation.data.get(remote_unit, None)
-            if unit_cache is not None:
-                unit_cache._invalidate()
-            self._model.relations._invalidate(relation_name)
+        remote_unit = self._model.get_unit(remote_unit_name)
+        relation = self._model.get_relation(relation_name, relation_id)
+        unit_cache = relation.data.get(remote_unit, None)
+        if unit_cache is not None:
+            unit_cache._invalidate()
+        self._model.relations._invalidate(relation_name)
         if self._charm is None or not self._hooks_enabled:
             return
         self._charm.on[relation_name].relation_joined.emit(
             relation, remote_unit.app, remote_unit)
+
+    def remove_relation_unit(self, relation_id: int, remote_unit_name: str) -> None:
+        """Remove a unit from a relation.
+
+        Example::
+
+          rel_id = harness.add_relation('db', 'postgresql')
+          harness.add_relation_unit(rel_id, 'postgresql/0')
+          ...
+          harness.remove_relation_unit(rel_id, 'postgresql/0')
+
+        This will trigger a `relation_departed` event. This would
+        normally be followed by a `relation_changed` event triggered
+        by Juju. However when using the test harness a
+        `relation_changed` event must be triggererd using
+        :meth:`.update_relation_data`. This deviation from normal Juju
+        behaviour, facilitates testing by making each step in the
+        charm life cycle explicit.
+
+        Args:
+            relation_id: The integer relation identifier (as returned by add_relation).
+            remote_unit_name: A string representing the remote unit that is being added.
+
+        Raises:
+            KeyError: if relation_id or remote_unit_name is not valid
+            ValueError: if remote_unit_name is not valid
+        """
+        relation_name = self._backend._relation_names[relation_id]
+
+        # gather data to invalidate cache later
+        remote_unit = self._model.get_unit(remote_unit_name)
+        relation = self._model.get_relation(relation_name, relation_id)
+        unit_cache = relation.data.get(remote_unit, None)
+
+        # statements which could access cache
+        self._emit_relation_departed(relation_id, remote_unit_name)
+        self._backend._relation_data[relation_id].pop(remote_unit_name)
+        self._backend._relation_app_and_units[relation_id][
+            "units"].remove(remote_unit_name)
+        self._backend._relation_list_map[relation_id].remove(remote_unit_name)
+
+        if unit_cache is not None:
+            unit_cache._invalidate()
+
+    def _emit_relation_departed(self, relation_id, unit_name):
+        """Trigger relation-departed event for a given relation id and unit."""
+        if self._charm is None or not self._hooks_enabled:
+            return
+        rel_name = self._backend._relation_names[relation_id]
+        relation = self.model.get_relation(rel_name, relation_id)
+        if '/' in unit_name:
+            app_name = unit_name.split('/')[0]
+            app = self.model.get_app(app_name)
+            unit = self.model.get_unit(unit_name)
+        else:
+            raise ValueError('Invalid Unit Name')
+        self._charm.on[rel_name].relation_departed.emit(relation, app, unit)
 
     def get_relation_data(self, relation_id: int, app_or_unit: str) -> typing.Mapping:
         """Get the relation data bucket for a single app or unit in a given relation.
