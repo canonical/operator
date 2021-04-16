4916c0de1ef9463b75b6531bbaaa5fbca57b5134
Jon Seager <jon@sgrs.uk>
2021-04-16 15:34:49 +0100

    Fixed trailing whitespace on docstring EOLs

--- ops/__init__.py
+++ ops/__init__.py
@@ -12,28 +12,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""The Charmed Operator Framework
+"""The Charmed Operator Framework.
 
 The Charmed Operator Framework allows the development of operators in a simple
-and straightforward way, using standard Python structures to allow for clean, 
+and straightforward way, using standard Python structures to allow for clean,
 maintainable, and reusable code.
 
-A Kubernetes operator is a container that drives lifecycle management, 
-configuration, integration and daily actions for an application. Operators 
-simplify software management and operations. They capture reusable app domain 
+A Kubernetes operator is a container that drives lifecycle management,
+configuration, integration and daily actions for an application. Operators
+simplify software management and operations. They capture reusable app domain
 knowledge from experts in a software component that can be shared.
 
-The Charmed Operator Framework extends the "operator pattern" to enable Charmed 
-Operators, packaged as and often referred to as “charms”. Charms are not just 
-for Kubernetes but also operators for traditional Linux or Windows application 
-management. Operators use an Operator Lifecycle Manager (OLM), like Juju, to 
-coordinate their work in a cluster. The system uses Golang for concurrent event 
+The Charmed Operator Framework extends the "operator pattern" to enable Charmed
+Operators, packaged as and often referred to as “charms”. Charms are not just
+for Kubernetes but also operators for traditional Linux or Windows application
+management. Operators use an Operator Lifecycle Manager (OLM), like Juju, to
+coordinate their work in a cluster. The system uses Golang for concurrent event
 processing under the hood, but enables the operators to be written in Python.
 
-Operators should do one thing and do it well. Each operator drives a single 
-application or service and can be composed with other operators to deliver a 
-complex application or service. An operator handles instantiation, scaling, 
-configuration, optimisation, networking, service mesh, observability, 
+Operators should do one thing and do it well. Each operator drives a single
+application or service and can be composed with other operators to deliver a
+complex application or service. An operator handles instantiation, scaling,
+configuration, optimisation, networking, service mesh, observability,
 and day-2 operations specific to that application.
 
 Full developer documentation is available at https://juju.is/docs/sdk.
