676831aa9c05c4aaccdda909a1a1953738032b1a
sed-i <82407168+sed-i@users.noreply.github.com>
2021-11-03 17:05:31 -0400

    cannot use base class inside Callable

--- ops/framework.py
+++ ops/framework.py
@@ -641,7 +641,7 @@ class Framework(Object):
         """Discard a persistent snapshot."""
         self._storage.drop_snapshot(handle.path)
 
-    def observe(self, bound_event: BoundEvent, observer: typing.Callable[[EventBase], None]):
+    def observe(self, bound_event: BoundEvent, observer: typing.Callable[[typing.Any], None]):
         """Register observer to be called when bound_event is emitted.
 
         The bound_event is generally provided as an attribute of the object that emits
