304fc34f02cabc1d276adbb736b8161f1c98581c
Ryan Barry <ryan.barry@canonical.com>
2021-11-03 18:07:51 +0000

    Pep8

--- test/test_charm.py
+++ test/test_charm.py
@@ -275,7 +275,8 @@ storage:
         charm.on['stor2'].storage_detaching.emit(Storage("stor2", 0, charm.model._backend))
         charm.on['stor3'].storage_attached.emit(Storage("stor3", 0, charm.model._backend))
         charm.on['stor-4'].storage_attached.emit(Storage("stor-4", 0, charm.model._backend))
-        charm.on['stor-multiple-dashes'].storage_attached.emit(Storage("stor-multiple-dashes", 0, charm.model._backend))
+        charm.on['stor-multiple-dashes'].storage_attached.emit(
+            Storage("stor-multiple-dashes", 0, charm.model._backend))
 
         self.assertEqual(charm.seen, [
             'StorageAttachedEvent',
