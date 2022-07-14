614f6c6a910f5d097b3e4b927e55986ea2e8b55c
Pen Gale <pete.vandergiessen@canonical.com>
2022-07-14 18:16:48 +0000

    Added a very basic smoke test. Run it with `tox -e smoke`
    This requires a development box with an active juju controller, and
    charmcraft installed.
    
    The smoke test will build the "smoke" charm in test/charms/test_smoke
    and try to deploy xenial, bionic and focal versions of the charm.
    
    There are a few wonky things about this, including how we inject the
    current ops package, and the fact that we test with just the focal
    version of the charm. Future versions of this smoke test should clear
    those wonky bits up, as well as add automation.

---
+++ test/charms/test_smoke/README.md
@@ -0,0 +1,9 @@
+# smoke
+
+## Description
+
+A simple test charm for running smoke tests.
+
+## Usage
+
+Run `tox -e smoke` in the root directory of this repository to build and deploy this charm.
---
+++ test/charms/test_smoke/src/charm.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# Copyright 2022 Penelope Valentine Gale
+# See LICENSE file for licensing details.
+#
+# Learn more at: https://juju.is/docs/sdk
+
+"""Charm the service.
+
+Refer to the following post for a quick-start guide that will help you
+develop a new k8s charm using the Operator Framework:
+
+    https://discourse.charmhub.io/t/4208
+"""
+
+import logging
+
+from ops.charm import CharmBase
+from ops.main import main
+from ops.model import ActiveStatus
+
+logger = logging.getLogger(__name__)
+
+
+class SmokeCharm(CharmBase):
+    """Charm the service."""
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.framework.observe(self.on.install, self._on_install)
+
+    def _on_install(self, event):
+        self.unit.status = ActiveStatus()
+
+if __name__ == "__main__":
+    main(SmokeCharm)
