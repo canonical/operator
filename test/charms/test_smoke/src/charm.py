7abc3039c485dd11741f33fa9e75eaef5a73db42
Pen Gale <pete.vandergiessen@canonical.com>
2022-08-10 16:04:40 -0400

    Added a very basic smoke test. Run it with `tox -e smoke` (#800)
    This requires a development box with an active juju controller, and
    charmcraft installed.
    
    The smoke test will build the "smoke" charm in test/charms/test_smoke
    and try to deploy xenial, bionic and focal versions of the charm.
    
    There are a few wonky things about this, including how we inject the
    current ops package, and the fact that we test with just the focal
    version of the charm. Future versions of this smoke test should clear
    those wonky bits up, as well as add automation.

---
+++ test/charms/test_smoke/src/charm.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+#
+# Copyright 2022 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Charm the service.
+
+Refer to the following post for a quick-start guide that will help you
+develop a new k8s charm using the Operator Framework:
+
+    https://discourse.charmhub.io/t/4208
+"""
+
+import logging
+
+from ops.charm import CharmBase
+from ops.main import main
+from ops.model import ActiveStatus
+
+logger = logging.getLogger(__name__)
+
+
+class SmokeCharm(CharmBase):
+    """Charm the service."""
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.framework.observe(self.on.install, self._on_install)
+
+    def _on_install(self, event):
+        self.unit.status = ActiveStatus()
+
+
+if __name__ == "__main__":
+    main(SmokeCharm)
