0f8832724758d5a4e89d044cbeee91b9b6d3c7d4
Pietro Pasotti <starfire.daemon@gmail.com>
2023-03-16 17:59:10 +0100

    link to gh

--- scenario/scripts/main.py
+++ scenario/scripts/main.py
@@ -20,7 +20,9 @@ def _setup_logging(verbosity: int):
 def main():
     app = typer.Typer(
         name="scenario",
-        help="Scenario utilities.",
+        help="Scenario utilities. "
+             "For docs, issues and feature requests, visit "
+             "the github repo --> https://github.com/canonical/ops-scenario",
         no_args_is_help=True,
         rich_markup_mode="markdown",
     )
