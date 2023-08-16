76328523e87ca14dfaa97aebf55e4b2faf1cbc75
Ben Hoyt <ben.hoyt@canonical.com>
2023-08-16 15:56:28 +1200

    Use f.read(0) for is-binary test instead of using abstract base classes

--- ops/testing.py
+++ ops/testing.py
@@ -2594,7 +2594,7 @@ class _TestingPebbleClient:
                 file_path.write_bytes(source)
             else:
                 # If source is binary, open file in binary mode and ignore encoding param
-                is_binary = isinstance(source, (io.RawIOBase, io.BufferedIOBase))
+                is_binary = isinstance(source.read(0), bytes)
                 open_mode = 'wb' if is_binary else 'w'
                 open_encoding = None if is_binary else encoding
                 with file_path.open(open_mode, encoding=open_encoding) as f:
