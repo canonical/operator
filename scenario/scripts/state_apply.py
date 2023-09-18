11aff053e728ae4cfe53260889f3c6a7f70b5d74
Pietro Pasotti <starfire.daemon@gmail.com>
2023-09-18 10:54:40 +0200

    removed headers from json output in snapshot

--- scenario/scripts/state_apply.py
+++ scenario/scripts/state_apply.py
@@ -230,7 +230,11 @@ def state_apply(
     Usage: state-apply myapp/0 > ./tests/scenario/case1.py
     """
     push_files_ = json.loads(push_files.read_text()) if push_files else None
-    state_ = json.loads(state.read_text())
+    state_json = json.loads(state.read_text())
+
+    # TODO: state_json to State
+    raise NotImplementedError("WIP: implement State.from_json")
+    state_: State = State.from_json(state_json)
 
     return _state_apply(
         target=target,
