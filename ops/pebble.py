3513f4c67bdbb51c0c7384f3b39365d8846734e5
Tony Meyer <tony.meyer@canonical.com>
2024-02-26 12:45:00 +1300

    Make ruff happy.

--- ops/pebble.py
+++ ops/pebble.py
@@ -179,13 +179,17 @@ _HeaderHandler = Callable[[bytes], None]
 
 class _Tempfile(Protocol):
     name = ''
+
     def write(self, data: bytes): ...
+
     def close(self): ...
 
 
 class _FileLikeIO(Protocol[typing.AnyStr]):  # That also covers TextIO and BytesIO
     def read(self, __n: int = ...) -> typing.AnyStr: ...  # for BinaryIO
+
     def write(self, __s: typing.AnyStr) -> int: ...
+
     def __enter__(self) -> typing.IO[typing.AnyStr]: ...
 
 
@@ -276,9 +280,13 @@ if TYPE_CHECKING:
 
 class _WebSocket(Protocol):
     def connect(self, url: str, socket: socket.socket): ...
+
     def shutdown(self): ...
+
     def send(self, payload: str): ...
+
     def send_binary(self, payload: bytes): ...
+
     def recv(self) -> Union[str, bytes]: ...
 
 
@@ -736,7 +744,7 @@ class Plan:
     """
 
     def __init__(self, raw: Optional[Union[str, 'PlanDict']]):
-        if isinstance(raw, str):
+        if isinstance(raw, str):  # noqa: SIM108
             d = yaml.safe_load(raw) or {}  # type: ignore
         else:
             d = raw or {}
