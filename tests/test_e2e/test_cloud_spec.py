108b1ce9512b8486d8ab67e8a0e81dab62cbb0b5
Tiexin Guo <guotiexin@gmail.com>
2024-04-08 08:38:56 +0800

    test: add e2e test for cloud spec

---
+++ tests/test_e2e/test_cloud_spec.py
@@ -0,0 +1,40 @@
+import ops
+import pytest
+
+import scenario
+
+
+@pytest.fixture(scope="function")
+def mycharm():
+    class MyCharm(ops.CharmBase):
+        def __init__(self, framework: ops.Framework):
+            super().__init__(framework)
+            for evt in self.on.events().values():
+                self.framework.observe(evt, self._on_event)
+
+        def _on_event(self, event):
+            pass
+
+    return MyCharm
+
+
+def test_get_cloud_spec(mycharm):
+    cloud_spec = ops.model.CloudSpec.from_dict(
+        {
+            "name": "localhost",
+            "type": "lxd",
+            "endpoint": "https://127.0.0.1:8443",
+            "credential": {
+                "auth-type": "certificate",
+                "attrs": {
+                    "client-cert": "foo",
+                    "client-key": "bar",
+                    "server-cert": "baz",
+                },
+            },
+        }
+    )
+
+    ctx = scenario.Context(mycharm, meta={"name": "foo"})
+    with ctx.manager("start", scenario.State(cloud_spec=cloud_spec)) as mgr:
+        assert mgr.charm.model.get_cloud_spec() == cloud_spec
