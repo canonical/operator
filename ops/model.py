ee7351b7d718f80df8e7cabded95c4f3165af1f6
Dima Tisnek <dimaqq@gmail.com>
2024-09-09 10:28:42 +0900

    more precise language
    Co-authored-by: Tony Meyer <tony.meyer@gmail.com>

--- ops/model.py
+++ ops/model.py
@@ -1445,9 +1445,8 @@ class Secret:
         secret no longer exists, this method will succeed, but the unit will go
         into error state on completion of the current Juju hook.
 
-        Changed in Juju 3.6: setting the content to the same exact value no
-        longer creates a new secret revision.
-
+        Changed in Juju 3.6: setting the content with the same content as the latest
+        revision longer creates a new secret revision.
         Args:
             content: A key-value mapping containing the payload of the secret,
                 for example :code:`{"password": "foo123"}`.
