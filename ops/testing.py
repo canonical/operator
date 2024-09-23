d6c7d2d93e51dfc54f652c2aae3bdf579a0fcf02
Tony Meyer <tony.meyer@canonical.com>
2024-09-23 17:19:34 +1200

    Use __all__ and move ActionFailed to Harness.

--- ops/testing.py
+++ ops/testing.py
@@ -32,11 +32,12 @@ The module includes:
     on testing charms, see `Charm SDK | Testing <https://juju.is/docs/sdk/testing>`_.
 """
 
-import typing as _typing
-from importlib.metadata import PackageNotFoundError as _PackageNotFoundError
-from importlib.metadata import version as _get_package_version
+# ruff: noqa: F401 (unused import)
+
+import importlib.metadata
 
 from ._private.harness import (
+    ActionFailed,
     ActionOutput,
     AppUnitOrName,
     CharmBase,
@@ -59,12 +60,26 @@ from ._private.harness import (
     storage,
 )
 
+# The Harness unit testing framework.
+__all__ = [
+    'ActionFailed',
+    'ActionOutput',
+    'AppUnitOrName',
+    'CharmType',
+    'ExecArgs',
+    'ExecHandler',
+    'ExecResult',
+    'Harness',
+    'ReadableBuffer',
+    'YAMLStringOrFile',
+]
+
 # If the 'ops.testing' optional extra is installed, make those
 # names available in this namespace.
 try:
-    _version = _get_package_version('ops-scenario')
-except _PackageNotFoundError:
-    from ops._private.harness import ActionFailed  # type: ignore
+    _version = importlib.metadata.version('ops-scenario')
+except importlib.metadata.PackageNotFoundError:
+    pass
 else:
     if _version and int(_version.split('.', 1)[0]) >= 7:
         from scenario import (
@@ -108,114 +123,76 @@ else:
             WaitingStatus,
             errors,
         )
+
+        # This can be imported in the group above after Scenario exposes it at the top level.
+        # https://github.com/canonical/ops-scenario/pull/200
         from scenario.context import CharmEvents
 
         # The Scenario unit testing framework.
-        _ = ActiveStatus
-        _ = Address
-        _ = AnyJson
-        _ = BindAddress
-        _ = BlockedStatus
-        _ = CharmEvents
-        _ = CheckInfo
-        _ = CloudCredential
-        _ = CloudSpec
-        _ = Container
-        _ = Context
-        _ = DeferredEvent
-        _ = ErrorStatus
-        _ = Exec
-        _ = ICMPPort
-        _ = JujuLogLine
-        _ = MaintenanceStatus
-        _ = Manager
-        _ = Model
-        _ = Mount
-        _ = Network
-        _ = Notice
-        _ = PeerRelation
-        _ = Port
-        _ = RawDataBagContents
-        _ = RawSecretRevisionContents
-        _ = Relation
-        _ = RelationBase
-        _ = Resource
-        _ = Secret
-        _ = State
-        _ = Storage
-        _ = StoredState
-        _ = SubordinateRelation
-        _ = TCPPort
-        _ = UDPPort
-        _ = UnitID
-        _ = UnknownStatus
-        _ = WaitingStatus
-        _ = errors
+        __all__.extend([
+            'ActiveStatus',
+            'Address',
+            'AnyJson',
+            'BindAddress',
+            'BlockedStatus',
+            'CharmEvents',
+            'CheckInfo',
+            'CloudCredential',
+            'CloudSpec',
+            'Container',
+            'Context',
+            'DeferredEvent',
+            'ErrorStatus',
+            'Exec',
+            'ICMPPort',
+            'JujuLogLine',
+            'MaintenanceStatus',
+            'Manager',
+            'Model',
+            'Mount',
+            'Network',
+            'Notice',
+            'PeerRelation',
+            'Port',
+            'RawDataBagContents',
+            'RawSecretRevisionContents',
+            'Relation',
+            'RelationBase',
+            'Resource',
+            'Secret',
+            'State',
+            'Storage',
+            'StoredState',
+            'SubordinateRelation',
+            'TCPPort',
+            'UDPPort',
+            'UnitID',
+            'UnknownStatus',
+            'WaitingStatus',
+            'errors',
+        ])
 
-        # Handle the name clash between Harness's and Scenario's ActionFailed.
-        class ActionFailed(Exception):  # noqa: N818
-            """Raised when :code:`event.fail()` is called during an action handler."""
-
-            message: str
-            """Optional details of the failure, as provided by :meth:`ops.ActionEvent.fail`."""
-
-            output: ActionOutput
-            """Any logs and results set by the Charm.
-
-            When using Context.run, both logs and results will be empty - these
-            can be found in Context.action_logs and Context.action_results.
-            """
-
-            state: _typing.Optional[State]
-            """The Juju state after the action has been run.
-
-            When using Harness.run_action, this will be None.
-            """
-
-            def __init__(
-                self,
-                message: str,
-                output: _typing.Optional[ActionOutput] = None,
-                *,
-                state: _typing.Optional[State] = None,
-            ):
-                self.message = message
-                self.output = ActionOutput([], {}) if output is None else output
-                self.state = state
-
-        # Monkeypatch this merged one in so that isinstance checks work.
-        import ops._private.harness as _harness
-
-        _harness.ActionFailed = ActionFailed
+        # Until Scenario uses the ops._private.harness.ActionFailed, we need to
+        # monkeypatch it in, so that the ops.testing.ActionFailed object is the
+        # one that we expect, even if people are mixing Harness and Scenario.
+        # https://github.com/canonical/ops-scenario/issues/201
         import scenario.context as _context
         import scenario.runtime as _runtime
 
         _context.ActionFailed = ActionFailed  # type: ignore[reportPrivateImportUsage]
         _runtime.ActionFailed = ActionFailed  # type: ignore[reportPrivateImportUsage]
-    else:
-        from ops._private.harness import ActionFailed  # type: ignore
-
-# The Harness unit testing framework.
-_ = ActionFailed  # If Scenario has been installed, then this will be the merged ActionFailed.
-_ = ActionOutput
-_ = AppUnitOrName
-_ = CharmType
-_ = ExecArgs
-_ = ExecHandler
-_ = ExecResult
-_ = Harness
-_ = ReadableBuffer
-_ = YAMLStringOrFile
 
 # Names exposed for backwards compatibility
-_ = CharmBase
-_ = CharmMeta
-_ = Container  # If Scenario has been installed, then this will be scenario.Container.
-_ = ExecProcess
-_ = RelationNotFoundError
-_ = RelationRole
-_ = charm
-_ = framework
-_ = model
-_ = pebble
-_ = storage
+__all__.extend([
+    'CharmBase',
+    'CharmMeta',
+    'Container',  # If Scenario has been installed, then this will be scenario.Container.
+    'ExecProcess',
+    'RelationNotFoundError',
+    'RelationRole',
+    'charm',
+    'framework',
+    'model',
+    'pebble',
+    'storage',
+])
