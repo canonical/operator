1a14be7b47c69c3749707c13358c82d5f11fedb9
Tony Meyer <tony.meyer@canonical.com>
2024-11-27 18:38:22 +1300

    Use the conf to install the local package.

--- docs/custom_conf.py
+++ docs/custom_conf.py
@@ -212,9 +212,7 @@ custom_extensions = [
 # pyspelling, sphinx, sphinx-autobuild, sphinx-copybutton, sphinx-design,
 # sphinx-notfound-page, sphinx-reredirects, sphinx-tabs, sphinxcontrib-jquery,
 # sphinxext-opengraph
-custom_required_modules = [
-    'testing/',
-]
+custom_required_modules = []
 
 # Add files or directories that should be excluded from processing.
 custom_excludes = [
