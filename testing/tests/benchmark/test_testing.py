b996b4bf898eb4abf01ba5c2a8f675ccb58696fc
Tony Meyer <tony.meyer@canonical.com>
2025-01-21 12:22:39 +1300

    Only patch sys.path for as long as needed.
    Also: move the charm to the Scenario test folder. Scenario needs it for benchmarking because the behaviour is different when it's a charm in actual files vs. a charm in the same file. ops doesn't distinguish between those case so we are unlikely to need this for ops benchmarking.

--- testing/tests/benchmark/test_testing.py
+++ testing/tests/benchmark/test_testing.py
@@ -18,20 +18,19 @@ import dataclasses
 import pathlib
 import sys
 
+import pytest
+
 import ops
 from ops import testing
 
-sys.path.append(
-    str(
-        pathlib.Path(__file__).parent.parent.parent.parent
-        / "test"
-        / "charms"
-        / "test_benchmark"
-        / "src"
-    )
-)
 
-from benchmark_charm import BenchmarkCharm
+@pytest.fixture
+def benchmark_charm():
+    charm_path = str(pathlib.Path(__file__).parent / "charms" / "benchmark_charm" / "src")
+    sys.path.append(charm_path)
+    from benchmark_charm import BenchmarkCharm
+    yield BenchmarkCharm
+    sys.path.remove(charm_path)
 
 
 # Note: the 'benchmark' argument here is a fixture that pytest-benchmark
@@ -41,8 +40,8 @@ def test_context_explicit_meta(benchmark):
     assert isinstance(ctx, testing.Context)
 
 
-def test_run_no_observer(benchmark):
-    ctx = testing.Context(BenchmarkCharm)
+def test_run_no_observer(benchmark, benchmark_charm):
+    ctx = testing.Context(benchmark_charm)
     benchmark(ctx.run, ctx.on.start(), testing.State())
     # As with all these tests, we're interested in how long the
     # test took to run. However, we also verify that we did emit
@@ -52,8 +51,8 @@ def test_run_no_observer(benchmark):
     assert len({e.handle.kind for e in ctx.emitted_events}) == 1
 
 
-def test_run_observed(benchmark):
-    ctx = testing.Context(BenchmarkCharm)
+def test_run_observed(benchmark, benchmark_charm):
+    ctx = testing.Context(benchmark_charm)
     benchmark(ctx.run, ctx.on.stop(), testing.State())
     assert len({e.handle.kind for e in ctx.emitted_events}) == 1
 
@@ -70,8 +69,8 @@ def test_context_explicit_meta_config_actions(benchmark):
     assert len({e.handle.kind for e in ctx.emitted_events}) == 1
 
 
-def test_context_autoload_meta(benchmark):
-    ctx = benchmark(testing.Context, BenchmarkCharm)
+def test_context_autoload_meta(benchmark, benchmark_charm):
+    ctx = benchmark(testing.Context, benchmark_charm)
     assert isinstance(ctx, testing.Context)
 
 
@@ -94,10 +93,10 @@ def test_many_tests_explicit_meta(benchmark):
     # sense to simulate multiple events in a row, and we want to make
     # sure that we are timing test that take this form as well, where the
     # cost of creating the Context object is amortised across many runs.
-    ctx = testing.Context(ops.CharmBase, meta={"name": "foo"})
 
     def mock_pytest():
         """Simulate running multiple tests against the same charm."""
+        ctx = testing.Context(ops.CharmBase, meta={"name": "foo"})
         state = testing.State()
         for event in ("install", "start", "stop", "remove"):
             state = ctx.run(getattr(ctx.on, event)(), state)
@@ -105,24 +104,24 @@ def test_many_tests_explicit_meta(benchmark):
     benchmark(mock_pytest)
 
 
-def test_many_tests_autoload_meta(benchmark):
+def test_many_tests_autoload_meta(benchmark, benchmark_charm):
     def mock_pytest():
         """Simulate running multiple tests against the same charm."""
+        ctx = testing.Context(benchmark_charm)
+        state = testing.State()
         for event in ("install", "start", "stop", "remove"):
-            for _ in range(5):
-                ctx = testing.Context(BenchmarkCharm)
-                ctx.run(getattr(ctx.on, event)(), testing.State())
+            state = ctx.run(getattr(ctx.on, event)(), state)
 
     benchmark(mock_pytest)
 
 
-def test_lots_of_logs(benchmark):
-    ctx = testing.Context(BenchmarkCharm)
+def test_lots_of_logs(benchmark, benchmark_charm):
+    ctx = testing.Context(benchmark_charm)
     benchmark(ctx.run, ctx.on.update_status(), testing.State())
     assert len(ctx.juju_log) > 200
 
 
-def test_full_state(benchmark):
+def test_full_state(benchmark, benchmark_charm):
     def fill_state():
         rel = testing.Relation("rel")
         peer = testing.PeerRelation("peer")
@@ -149,7 +148,7 @@ def test_full_state(benchmark):
         )
         return state
 
-    ctx = testing.Context(BenchmarkCharm)
+    ctx = testing.Context(benchmark_charm)
     state_in = benchmark(fill_state)
     state_out = ctx.run(ctx.on.start(), state_in)
     # stored_states is complicated: it will contain a stored state the
@@ -167,9 +166,9 @@ def test_full_state(benchmark):
     assert state_in_dict == state_out_dict
 
 
-def test_deferred_events(benchmark):
-    ctx = testing.Context(BenchmarkCharm, capture_deferred_events=True)
-    deferred = ctx.on.stop().deferred(BenchmarkCharm._on_stop)
+def test_deferred_events(benchmark, benchmark_charm):
+    ctx = testing.Context(benchmark_charm, capture_deferred_events=True)
+    deferred = ctx.on.stop().deferred(benchmark_charm._on_stop)
     state_in = testing.State(deferred=[deferred])
     state_out = benchmark(ctx.run, ctx.on.config_changed(), state_in)
     assert len(state_out.deferred) == 1
