c1be796d38119465c271780c1d2fb9e701571427
Tony Meyer <tony.meyer@canonical.com>
2025-02-05 10:33:07 +1300

    For now, link to charmcraft latest, as it has the anchors we need.

--- docs/custom_conf.py
+++ docs/custom_conf.py
@@ -298,7 +298,7 @@ autodoc_default_options = {
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3', None),
     'juju': ('https://canonical-juju.readthedocs-hosted.com/en/latest/', None),
-    'charmcraft': ('https://canonical-charmcraft.readthedocs-hosted.com/en/stable/', None),
+    'charmcraft': ('https://canonical-charmcraft.readthedocs-hosted.com/en/latest/', None),
     }
 
 # -- General configuration ---------------------------------------------------
