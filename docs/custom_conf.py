e54b7662d3c95e197b666c30026d63d62f853634
Tony Meyer <tony.meyer@canonical.com>
2025-02-05 10:54:21 +1300

    Remove the trailing slash, which is not required.

--- docs/custom_conf.py
+++ docs/custom_conf.py
@@ -297,8 +297,8 @@ autodoc_default_options = {
 # that should be linked to in this documentation.
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3', None),
-    'juju': ('https://canonical-juju.readthedocs-hosted.com/en/latest/', None),
-    'charmcraft': ('https://canonical-charmcraft.readthedocs-hosted.com/en/latest/', None),
+    'juju': ('https://canonical-juju.readthedocs-hosted.com/en/latest', None),
+    'charmcraft': ('https://canonical-charmcraft.readthedocs-hosted.com/en/latest', None),
 }
 
 # -- General configuration ---------------------------------------------------
