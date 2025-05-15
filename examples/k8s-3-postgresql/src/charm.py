35f04b53e804f489fcfbb2d518f225b3f5f81584
David Wilding <david.wilding@canonical.com>
2025-05-15 07:54:08 +0200

    add charm before chapter 3

---
+++ examples/k8s-3-postgresql/src/charm.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+
+# Copyright 2025 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Kubernetes charm for a demo app."""
+
+import logging
+
+import ops
+
+# Log messages can be retrieved using juju debug-log
+logger = logging.getLogger(__name__)
+
+
+class FastAPIDemoCharm(ops.CharmBase):
+    """Charm the service."""
+
+    def __init__(self, framework: ops.Framework) -> None:
+        super().__init__(framework)
+        framework.observe(self.on.demo_server_pebble_ready, self._on_demo_server_pebble_ready)
+        framework.observe(self.on.config_changed, self._on_config_changed)
+        # see 'containers' in charmcraft.yaml
+        self.container = self.unit.get_container('demo-server')
+        self.pebble_service_name = 'fastapi-service'
+
+    def _on_demo_server_pebble_ready(self, event: ops.PebbleReadyEvent) -> None:
+        self._update_layer_and_restart()
+
+    def _on_config_changed(self, event: ops.ConfigChangedEvent) -> None:
+        port = self.config['server-port']  # See charmcraft.yaml
+
+        if port == 22:
+            self.unit.status = ops.BlockedStatus('invalid port number, 22 is reserved for SSH')
+            return
+
+        logger.debug('New application port is requested: %s', port)
+        self._update_layer_and_restart()
+
+    def _update_layer_and_restart(self) -> None:
+        """Define and start a workload using the Pebble API.
+
+        You'll need to specify the right entrypoint and environment
+        configuration for your specific workload. Tip: you can see the
+        standard entrypoint of an existing container using docker inspect
+        Learn more about interacting with Pebble at
+            https://ops.readthedocs.io/en/latest/reference/pebble.html
+        Learn more about Pebble layers at
+            https://documentation.ubuntu.com/pebble/how-to/use-layers/
+        """
+        # Learn more about statuses at
+        # https://documentation.ubuntu.com/juju/3.6/reference/status/
+        self.unit.status = ops.MaintenanceStatus('Assembling Pebble layers')
+        try:
+            self.container.add_layer('fastapi_demo', self._pebble_layer, combine=True)
+            logger.info("Added updated layer 'fastapi_demo' to Pebble plan")
+
+            # Tell Pebble to incorporate the changes, including restarting the
+            # service if required.
+            self.container.replan()
+            logger.info(f"Replanned with '{self.pebble_service_name}' service")
+
+            self.unit.status = ops.ActiveStatus()
+        except (ops.pebble.APIError, ops.pebble.ConnectionError):
+            self.unit.status = ops.MaintenanceStatus('Waiting for Pebble in workload container')
+
+    @property
+    def _pebble_layer(self) -> ops.pebble.Layer:
+        """A Pebble layer for the FastAPI demo services."""
+        command = ' '.join([
+            'uvicorn',
+            'api_demo_server.app:app',
+            '--host=0.0.0.0',
+            f'--port={self.config["server-port"]}',
+        ])
+        pebble_layer: ops.pebble.LayerDict = {
+            'summary': 'FastAPI demo service',
+            'description': 'pebble config layer for FastAPI demo server',
+            'services': {
+                self.pebble_service_name: {
+                    'override': 'replace',
+                    'summary': 'fastapi demo',
+                    'command': command,
+                    'startup': 'enabled',
+                }
+            },
+        }
+        return ops.pebble.Layer(pebble_layer)
+
+
+if __name__ == '__main__':  # pragma: nocover
+    ops.main(FastAPIDemoCharm)
