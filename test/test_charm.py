cd8069d031b389448895a39e11306057340dee45
Tony Meyer <tony.meyer@canonical.com>
2025-06-11 10:35:34 +1200

    Skip alias test for pydantic that doesn't support it.

--- test/test_charm.py
+++ test/test_charm.py
@@ -1919,6 +1919,11 @@ if pydantic is not None:
 def test_action_custom_naming_pattern(
     action_params: dict[str, int], action_class: type[object], request: pytest.FixtureRequest
 ):
+    # The latest version of Pydantic available for Python 3.8 does not support
+    # the `alias` metadata, so we need to skip the test for that case.
+    if pydantic is not None and action_class is _PydanticDataclassesAlias:
+        pytest.skip('Pydantic does not support dataclasses alias metadata in this version.')
+
     class Charm(ops.CharmBase):
         def __init__(self, framework: ops.Framework):
             super().__init__(framework)
