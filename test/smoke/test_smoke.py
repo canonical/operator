ec5839d4a3ae5514234896a5266ae3b00d8ce0bb
Tony Meyer <tony.meyer@canonical.com>
2025-06-12 15:17:47 +1200

    Always use charmcraft 3.

--- test/smoke/test_smoke.py
+++ test/smoke/test_smoke.py
@@ -71,8 +71,8 @@ def pack(charm_dir: pathlib.Path):
 @pytest.mark.parametrize(
     'base,charmcraft_version,name',
     (
-        ('20.04', 2, 'focal'),
-        ('22.04', 2, 'jammy'),
+        ('20.04', 3, 'jammy'),
+        ('22.04', 3, 'jammy'),
         ('24.04', 3, 'noble'),
     ),
 )
