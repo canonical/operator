4e4bda6da3b3c3af9483b0b19bca1d3695389178
Dave Wilding <david.wilding@canonical.com>
2025-06-13 09:57:34 +0800

    docs: add httpbin charm from Charmcraft as an example charm (#1743)
    In Charmcraft 4.0, we're planning to remove the `simple` profile of
    `charmcraft init` (see
    https://github.com/canonical/charmcraft/pull/2295). This PR adds
    `simple`'s charm as a demo of Ops, so that the charm isn't lost.
    
    I took the charm that was generated by `charmcraft init` and
    changed/removed placeholder comments & files that were more suitable for
    a template charm. I also made sure that the charm is formatted correctly
    for the Ops repo and passes our linting.
    
    Within the charm directory, I tested that the following work:
    
    - `tox -e static` - For now, this runs static checks on _src/charm.py_
    only. When we make future improvements including switching to uv (see
    #[1805](https://github.com/canonical/operator/issues/1805)) we could
    expand `static` to cover the testing code too.
    
    - `tox -e unit` and `tox -e integration`
    
    - Manual pack and deploy, as documented in _examples/README.md_ and (for
    the time being) _README.md_.
    
    I also added the charm to our CI job that runs unit tests for the
    example charms.
    
    ---
    
    Follow-up items in future PRs:
    
    - https://github.com/canonical/operator/issues/1655
    - https://github.com/canonical/operator/issues/1805

---
+++ examples/httpbin-demo/tests/unit/test_charm.py
@@ -0,0 +1,102 @@
+# Copyright 2025 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from charm import HttpbinDemoCharm
+
+import ops
+from ops import testing
+
+
+def test_httpbin_pebble_ready():
+    # Arrange:
+    ctx = testing.Context(HttpbinDemoCharm)
+    container = testing.Container('httpbin', can_connect=True)
+    state_in = testing.State(containers={container})
+
+    # Act:
+    state_out = ctx.run(ctx.on.pebble_ready(container), state_in)
+
+    # Assert:
+    updated_plan = state_out.get_container(container.name).plan
+    expected_plan = {
+        'services': {
+            'httpbin': {
+                'override': 'replace',
+                'summary': 'httpbin',
+                'command': 'gunicorn -b 0.0.0.0:80 httpbin:app -k gevent',
+                'startup': 'enabled',
+                'environment': {'GUNICORN_CMD_ARGS': '--log-level info'},
+            }
+        },
+    }
+    assert expected_plan == updated_plan
+    assert (
+        state_out.get_container(container.name).service_statuses['httpbin']
+        == ops.pebble.ServiceStatus.ACTIVE
+    )
+    assert state_out.unit_status == testing.ActiveStatus()
+
+
+def test_config_changed_valid_can_connect():
+    """Test a config-changed event when the config is valid and the container can be reached."""
+    # Arrange:
+    ctx = testing.Context(HttpbinDemoCharm)  # The default config will be read from charmcraft.yaml
+    container = testing.Container('httpbin', can_connect=True)
+    state_in = testing.State(
+        containers={container},
+        config={'log-level': 'debug'},  # This is the config the charmer passed with `juju config`
+    )
+
+    # Act:
+    state_out = ctx.run(ctx.on.config_changed(), state_in)
+
+    # Assert:
+    updated_plan = state_out.get_container(container.name).plan
+    gunicorn_args = updated_plan.services['httpbin'].environment['GUNICORN_CMD_ARGS']
+    assert gunicorn_args == '--log-level debug'
+    assert state_out.unit_status == testing.ActiveStatus()
+
+
+def test_config_changed_valid_cannot_connect():
+    """Test a config-changed event when the config is valid but the container cannot be reached.
+
+    We expect to end up in MaintenanceStatus waiting for the deferred event to
+    be retried.
+    """
+    # Arrange:
+    ctx = testing.Context(HttpbinDemoCharm)
+    container = testing.Container('httpbin', can_connect=False)
+    state_in = testing.State(containers={container}, config={'log-level': 'debug'})
+
+    # Act:
+    state_out = ctx.run(ctx.on.config_changed(), state_in)
+
+    # Assert:
+    assert isinstance(state_out.unit_status, testing.MaintenanceStatus)
+
+
+def test_config_changed_invalid():
+    """Test a config-changed event when the config is invalid."""
+    # Arrange:
+    ctx = testing.Context(HttpbinDemoCharm)
+    container = testing.Container('httpbin', can_connect=True)
+    invalid_level = 'foobar'
+    state_in = testing.State(containers={container}, config={'log-level': invalid_level})
+
+    # Act:
+    state_out = ctx.run(ctx.on.config_changed(), state_in)
+
+    # Assert:
+    assert isinstance(state_out.unit_status, testing.BlockedStatus)
+    assert invalid_level in state_out.unit_status.message
