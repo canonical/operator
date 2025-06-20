33f7e0e4d22443cdce3c406920d24ef2f30bc66e
David Wilding <david.wilding@canonical.com>
2025-06-20 09:08:25 +0800

    add constants for container name and service name

--- examples/httpbin-demo/src/charm.py
+++ examples/httpbin-demo/src/charm.py
@@ -25,6 +25,8 @@ import ops
 logger = logging.getLogger(__name__)
 
 VALID_LOG_LEVELS = ['info', 'debug', 'warning', 'error', 'critical']
+CONTAINER_NAME = 'httpbin'  # Name of workload container.
+SERVICE_NAME = 'httpbin'  # Name of Pebble service that runs in the workload container.
 
 
 class HttpbinDemoCharm(ops.CharmBase):
@@ -33,17 +35,17 @@ class HttpbinDemoCharm(ops.CharmBase):
     def __init__(self, framework: ops.Framework):
         super().__init__(framework)
         framework.observe(self.on.collect_unit_status, self._on_collect_status)
-        framework.observe(self.on['httpbin'].pebble_ready, self._on_httpbin_pebble_ready)
+        framework.observe(self.on[CONTAINER_NAME].pebble_ready, self._on_httpbin_pebble_ready)
         framework.observe(self.on.config_changed, self._on_config_changed)
         self.log_level = str(self.config['log-level']).lower()  # str() is for the type checker.
-        self.container = self.unit.get_container('httpbin')
+        self.container = self.unit.get_container(CONTAINER_NAME)
 
     def _on_collect_status(self, event: ops.CollectStatusEvent):
         """Report the status of the workload (runs after each event)."""
         if self.log_level not in VALID_LOG_LEVELS:
             event.add_status(ops.BlockedStatus(f"invalid log level: '{self.log_level}'"))
         try:
-            if not self.container.get_service('httpbin').is_running():
+            if not self.container.get_service(SERVICE_NAME).is_running():
                 event.add_status(ops.MaintenanceStatus('waiting for workload'))
         except (ops.pebble.ConnectionError, ops.ModelError):
             event.add_status(ops.MaintenanceStatus('waiting for workload container'))
@@ -89,7 +91,7 @@ class HttpbinDemoCharm(ops.CharmBase):
             'summary': 'httpbin layer',
             'description': 'pebble config layer for httpbin',
             'services': {
-                'httpbin': {
+                SERVICE_NAME: {
                     'override': 'replace',
                     'summary': 'httpbin',
                     'command': 'gunicorn -b 0.0.0.0:80 httpbin:app -k gevent',
