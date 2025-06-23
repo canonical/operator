4b01366646168f04bbf9842c9c9524e33010332a
Dima Tisnek <dima.tisnek@canonical.com>
2025-06-23 14:14:12 +0900

    minimal unit test

---
+++ testing/tests/test_e2e/test_trace_data.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+import ops
+
+from scenario import Context, State
+
+META = {
+    'name': 'traced_charm',
+    'requires': {'charm-tracing': {'interface': 'tracing', 'limit': 1}},
+}
+
+
+class TracedCharm(ops.CharmBase):
+    META = {
+        'name': 'traced_charm',
+        'requires': {'charm-tracing': {'interface': 'tracing'}},
+    }
+
+    def __init__(self, framework: ops.Framework):
+        super().__init__(framework)
+        self.tracing = ops.tracing.Tracing(self, 'charm-tracing')
+        for evt in self.on.events().values():
+            self.framework.observe(evt, self._on_event)
+
+    def _on_event(self, event: ops.EventBase):
+        pass
+
+
+def test_trace_data():
+    ctx = Context(TracedCharm, meta=META)
+    ctx.run(ctx.on.start(), State())
+    main_span = next(s for s in ctx.trace_data if s.name == 'ops.main')
+    assert not main_span.parent
+    assert {e.name for e in main_span.events} == {
+        'StartEvent',
+        'CollectStatusEvent',
+        'PreCommitEvent',
+        'CommitEvent',
+    }
