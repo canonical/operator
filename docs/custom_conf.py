7935a0d0c1a7d0658fad0ed0661870a090bcc6c2
David Wilding <david.wilding@canonical.com>
2025-07-04 13:10:00 +0800

    redo project name change

--- docs/custom_conf.py
+++ docs/custom_conf.py
@@ -62,7 +62,7 @@ sphinx.ext.autodoc.py_ext_sig_re = re.compile(
 ############################################################
 
 # Product name
-project = 'The ops library'
+project = 'Ops'
 author = 'Canonical Ltd.'
 
 # The title you want to display for the documentation in the sidebar.
