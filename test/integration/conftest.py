8862daab5e8f9a25d8da038056c993e6952245a6
David Wilding <david.wilding@canonical.com>
2025-08-01 06:52:34 +0800

    remove links from package testing code/config

--- test/integration/conftest.py
+++ test/integration/conftest.py
@@ -11,9 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# Learn more about testing at
-# https://documentation.ubuntu.com/ops/latest/explanation/testing.html
+
 
 from __future__ import annotations
 
