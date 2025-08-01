1908e525c18823020d6bc611cc19f1c49d8c997b
Dave Wilding <david.wilding@canonical.com>
2025-08-01 09:51:10 +0800

    docs: for 2.x, fix site title and unstyled error pages (#1945)
    After merging #1942, I noticed a couple of issues with the [2.x
    docs](https://documentation.ubuntu.com/ops/2.x/):
    
    - The site title should be "Ops 2.x documentation" not "Ops 2.23
    documentation"
    - Error pages are still unstyled
    ([example](https://documentation.ubuntu.com/ops/2.x/foobar)).
    Unfortunately I can't test fixes without building the live RTD docs. But
    I think this PR should fix it - we'll find out after it merges

--- docs/custom_conf.py
+++ docs/custom_conf.py
@@ -68,7 +68,7 @@ author = 'Canonical Ltd.'
 # The title you want to display for the documentation in the sidebar.
 # You might want to include a version number here.
 # To not display any title, set this option to an empty string.
-project_version = '2.23'
+project_version = '2.x'
 html_title = f'{project} {project_version} documentation'
 
 # The default value uses the current year as the copyright year.
@@ -150,7 +150,7 @@ if os.environ.get('READTHEDOCS', '') == 'True':
     html_context['github_user'] = 'canonical'
     html_context['github_repo'] = 'operator'
     html_context['github_version'] = '2.23-maintenance'
-    html_context['slug'] = 'operator'
+    html_context['slug'] = 'ops'
 
 # If your project is on documentation.ubuntu.com, specify the project
 # slug (for example, "lxd") here.
