b8e4b221040ebd5ea7547b97f025e24b5f7868cc
Dima Tisnek <dima.tisnek@canonical.com>
2025-08-13 16:47:17 +0900

    docs: update the httpbin charm to jubilant

---
+++ examples/httpbin-demo/tests/integration/conftest.py
@@ -0,0 +1,37 @@
+# Copyright 2025 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pathlib
+import subprocess
+
+import jubilant
+import pytest
+
+
+@pytest.fixture(scope='module')
+def juju(request: pytest.FixtureRequest):
+    with jubilant.temp_model() as juju:
+        juju.wait_timeout = 100
+
+        yield juju
+
+        if request.session.testsfailed:
+            log = juju.debug_log(limit=1000)
+            print(log, end='')
+
+
+@pytest.fixture(scope='session')
+def charm():
+    subprocess.check_call(['charmcraft', 'pack'])  # noqa
+    return next(pathlib.Path(".").glob("*.charm"))
