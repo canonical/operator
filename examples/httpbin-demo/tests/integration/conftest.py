dcd1d4713b02a3aee9477333da75478874c0598a
Dima Tisnek <dima.tisnek@canonical.com>
2025-08-18 11:50:45 +0900

    ruff format

--- examples/httpbin-demo/tests/integration/conftest.py
+++ examples/httpbin-demo/tests/integration/conftest.py
@@ -22,7 +22,6 @@ import pytest
 @pytest.fixture(scope='module')
 def juju(request: pytest.FixtureRequest):
     with jubilant.temp_model() as juju:
-
         yield juju
 
         if request.session.testsfailed:
