f75844435286e72aa5b2b9d009a8c1c5bca37e36
Tony Meyer <tony.meyer@canonical.com>
2025-08-21 10:26:04 +1200

    Fix the expected charmcraft location.

--- .github/actions/update-best-practice-table/main.py
+++ .github/actions/update-best-practice-table/main.py
@@ -105,7 +105,7 @@ def main():
     parser.add_argument(
         '--path-to-charmcraft',
         type=pathlib.Path,
-        default=pathlib.Path(__file__).parent.parent.parent / 'charmcraft',
+        default=pathlib.Path(__file__).parent.parent.parent.parent / 'charmcraft',
         help='Path to a clone of canonical/charmcraft',
     )
     args = parser.parse_args()
