1566512716b4a94fe02a8f54e1f2d92120160dc2
Tony Meyer <tony.meyer@canonical.com>
2025-08-20 22:21:29 +1200

    Put the environment variable names in backticks.

--- ops/jujucontext.py
+++ ops/jujucontext.py
@@ -50,13 +50,13 @@ class JujuContext:
     dispatch_path: str
     """The dispatch path in the format of 'actions/do-something'.
 
-    For example 'hooks/workload-pebble-ready' (from JUJU_DISPATCH_PATH).
+    For example 'hooks/workload-pebble-ready' (from ``JUJU_DISPATCH_PATH``).
     """
 
     hook_name: str
     """The name of the hook.
 
-    For example, 'install' (from JUJU_HOOK_NAME). Note that this is the empty
+    For example, 'install' (from ``JUJU_HOOK_NAME``). Note that this is the empty
     string for actions, and for relation, storage, and workload hooks the name
     will be prefixed with the name of the relation, storage, or workload.
     """
@@ -64,25 +64,25 @@ class JujuContext:
     model_name: str
     """The name of the model.
 
-    For example 'foo' (from JUJU_MODEL_NAME).
+    For example 'foo' (from ``JUJU_MODEL_NAME``).
     """
 
     model_uuid: str
     """The uuid of the model.
 
-    For example 'cdac5656-2423-4388-8f30-41854b4cca7d' (from JUJU_MODEL_UUID).
+    For example 'cdac5656-2423-4388-8f30-41854b4cca7d' (from ``JUJU_MODEL_UUID``).
     """
 
     unit_name: str
     """The name of the unit.
 
-    For example 'myapp/0' (from JUJU_UNIT_NAME).
+    For example 'myapp/0' (from ``JUJU_UNIT_NAME``).
     """
 
     version: JujuVersion
     """The version of Juju.
 
-    For example '3.4.0' (from JUJU_VERSION).
+    For example '3.4.0' (from ``JUJU_VERSION``).
     """
 
     # These variables are expected to be set in all hooks, but have reasonable defaults.
@@ -90,7 +90,7 @@ class JujuContext:
     availability_zone: str | None = None
     """The availability zone.
 
-    For example, 'zone1' (from JUJU_AVAILABILITY_ZONE).
+    For example, 'zone1' (from ``JUJU_AVAILABILITY_ZONE``).
     """
 
     charm_dir: Path = dataclasses.field(
@@ -98,31 +98,31 @@ class JujuContext:
     )
     """The root directory of the charm where it is running.
 
-    For example '/var/lib/juju/agents/unit-bare-0/charm' (from JUJU_CHARM_DIR).
+    For example '/var/lib/juju/agents/unit-bare-0/charm' (from ``JUJU_CHARM_DIR``).
     """
 
     debug: bool = False
     """Debug mode.
 
-    If true, write logs to stderr as well as to juju-log (from JUJU_DEBUG).
+    If true, write logs to stderr as well as to juju-log (from ``JUJU_DEBUG``).
     """
 
     debug_at: set[str] = dataclasses.field(default_factory=set[str])
     """Where you want to stop when debugging.
 
-    For example 'all' (from JUJU_DEBUG_AT).
+    For example 'all' (from ``JUJU_DEBUG_AT``).
     """
 
     machine_id: int | None = None
     """The ID of the machine.
 
-    For example, 1 (from JUJU_MACHINE_ID).
+    For example, 1 (from ``JUJU_MACHINE_ID``).
     """
 
     principal_unit: str | None = None
     """The name of the principal unit.
 
-    For example, 'myapp/0' (from JUJU_PRINCIPAL_UNIT).
+    For example, 'myapp/0' (from ``JUJU_PRINCIPAL_UNIT``).
     """
 
     # The remaining variables are all context for specific types of events.
@@ -130,106 +130,106 @@ class JujuContext:
     action_name: str | None = None
     """The action's name, for action events.
 
-    For example 'backup' (from JUJU_ACTION_NAME).
+    For example 'backup' (from ``JUJU_ACTION_NAME``).
     """
 
     action_uuid: str | None = None
     """The action's uuid, for action events.
 
-    For example '1' (from JUJU_ACTION_UUID).
+    For example '1' (from ``JUJU_ACTION_UUID``).
     """
 
     notice_id: str | None = None
     """The ID of the notice, for Pebble notice events.
 
-    For example '1', (from JUJU_NOTICE_ID).
+    For example '1', (from ``JUJU_NOTICE_ID``).
     """
 
     notice_key: str | None = None
     """The key of the notice, for Pebble notice events.
 
-    For example 'example.com/a', (from JUJU_NOTICE_KEY).
+    For example 'example.com/a', (from ``JUJU_NOTICE_KEY``).
     """
 
     notice_type: str | None = None
     """The type of the notice, for Pebble notice events.
 
-    For example 'custom' (from JUJU_NOTICE_TYPE).
+    For example 'custom' (from ``JUJU_NOTICE_TYPE``).
     """
 
     pebble_check_name: str | None = None
     """The name of the pebble check, for Pebble check events.
 
-    For example 'http-check' (from JUJU_PEBBLE_CHECK_NAME).
+    For example 'http-check' (from ``JUJU_PEBBLE_CHECK_NAME``).
     """
 
     relation_departing_unit_name: str | None = None
     """The unit that is departing a relation, for relation events.
 
-    For example 'remote/42' (from JUJU_DEPARTING_UNIT).
+    For example 'remote/42' (from ``JUJU_DEPARTING_UNIT``).
     """
 
     relation_name: str | None = None
     """The name of the relation, for relation events.
 
-    For example 'database' (from JUJU_RELATION).
+    For example 'database' (from ``JUJU_RELATION``).
     """
 
     relation_id: int | None = None
     """The id of the relation, for relation events.
 
     For example 1 (integer) if the original environment variable's value is 'database:1'
-    (from JUJU_RELATION_ID).
+    (from ``JUJU_RELATION_ID``).
     """
 
     remote_app_name: str | None = None
     """The name of the remote app, for relation events.
 
-    For example 'remoteapp1' (from JUJU_REMOTE_APP).
+    For example 'remoteapp1' (from ``JUJU_REMOTE_APP``).
     """
 
     remote_unit_name: str | None = None
     """The name of the remote unit, for relation events.
 
-    For example 'remoteapp1/0' (from JUJU_REMOTE_UNIT).
+    For example 'remoteapp1/0' (from ``JUJU_REMOTE_UNIT``).
     """
 
     secret_id: str | None = None
     """The ID of the secret, for secret events.
 
-    For example 'secret:dcc7aa9c-7202-4da6-8d5f-0fbbaa4e1a41' (from JUJU_SECRET_ID).
+    For example 'secret:dcc7aa9c-7202-4da6-8d5f-0fbbaa4e1a41' (from ``JUJU_SECRET_ID``).
     """
 
     secret_label: str | None = None
     """The label of the secret, for secret events.
 
-    For example 'db-password' (from JUJU_SECRET_LABEL).
+    For example 'db-password' (from ``JUJU_SECRET_LABEL``).
     """
 
     secret_revision: int | None = None
     """The revision of the secret, for secret events.
 
-    For example 42 (integer) (from JUJU_SECRET_REVISION).
+    For example 42 (integer) (from ``JUJU_SECRET_REVISION``).
     """
 
     storage_index: int | None = None
     """The storage index, for storage events.
 
     For example 1 (integer) if the original environment variable's value is 'my-storage/1'
-    (from JUJU_STORAGE_ID).
+    (from ``JUJU_STORAGE_ID``).
     """
 
     storage_name: str | None = None
     """The storage name, for storage events.
 
     For example 'my-storage' if the original environment variable's value is 'my-storage/1'
-    (from JUJU_STORAGE_ID).
+    (from ``JUJU_STORAGE_ID``).
     """
 
     workload_name: str | None = None
     """The name of the workload, for workload events.
 
-    For example 'workload' (from JUJU_WORKLOAD_NAME).
+    For example 'workload' (from ``JUJU_WORKLOAD_NAME``).
     """
 
     @classmethod
