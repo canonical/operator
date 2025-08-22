cad57854a2fd297ce238397c83fd477f702d44e9
David Wilding <david.wilding@canonical.com>
2025-08-22 12:05:36 +0800

    change formatting per review

--- examples/httpbin-demo/tests/integration/test_charm.py
+++ examples/httpbin-demo/tests/integration/test_charm.py
@@ -38,11 +38,7 @@ def test_build_and_deploy(charm: Path, juju: jubilant.Juju):
 
 def test_block_on_invalid_config(charm: Path, juju: jubilant.Juju):
     """Check that the charm goes into blocked status if log-level is invalid."""
-    juju.config(
-        APP_NAME,
-        {
-            'log-level': 'foo'  # Should be one of info, debug, and so on.
-        },
-    )
+    # The value of log-level should be one of info, debug, and so on.
+    juju.config(APP_NAME, {'log-level': 'foo'})
     juju.wait(jubilant.all_blocked)
     juju.config(APP_NAME, reset='log-level')
