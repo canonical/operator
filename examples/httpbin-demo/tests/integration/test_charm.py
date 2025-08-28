f814668057e1e11a20f13a0c69c3b1d6ba26b4d7
David Wilding <david.wilding@canonical.com>
2025-08-28 10:17:04 +0800

    drop ./ from charmcraft.yaml

--- examples/httpbin-demo/tests/integration/test_charm.py
+++ examples/httpbin-demo/tests/integration/test_charm.py
@@ -20,7 +20,7 @@ import yaml
 
 logger = logging.getLogger(__name__)
 
-METADATA = yaml.safe_load(Path("./charmcraft.yaml").read_text())
+METADATA = yaml.safe_load(Path("charmcraft.yaml").read_text())
 APP_NAME = METADATA["name"]
 
 
