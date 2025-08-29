ffca2be15d8e8fee955bbfcf9fcbd614172fe559
Dima Tisnek <dima.tisnek@canonical.com>
2025-08-29 15:14:45 +0900

    chore: Friday item, rewrite vendored charm lib in pure Python

---
+++ tracing/test/test_equiavlence.py
@@ -0,0 +1,97 @@
+# Copyright 2025 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+import json
+from typing import Type, TypeAlias
+from unittest.mock import Mock
+
+import ops
+import ops.testing
+import pytest
+
+_pydantic = pytest.importorskip('pydantic')
+
+pytestmark = pytest.mark.filterwarnings('ignore::pydantic.PydanticDeprecatedSince20')
+
+
+def test_charm_runs(sample_charm: Type[ops.CharmBase]):
+    ctx = ops.testing.Context(sample_charm)
+    state_in = ops.testing.State()
+    state_out = ctx.run(ctx.on.start(), state_in)
+    assert isinstance(state_out.unit_status, ops.ActiveStatus)
+    assert 'ops.main' in [span.name for span in ctx.trace_data]
+    assert 'my collect status' in [span.name for span in ctx.trace_data]
+
+
+@pytest.fixture
+def mock_destination(monkeypatch: pytest.MonkeyPatch) -> Mock:
+    rv = Mock()
+    monkeypatch.setattr(ops.tracing, 'set_destination', rv)
+    return rv
+
+
+RECEIVER: TypeAlias = dict[str, int | str | dict[str, str]]
+RECEIVERS: TypeAlias = list[RECEIVER]
+GOOD_PROTOCOL: RECEIVER = {'protocol': {'name': 'otlp_http', 'type': 'http'}}
+GOOD_HTTP_URL: RECEIVER = {'url': 'http://tracing.example:4318/'}
+GOOD_HTTPS_URL: RECEIVER = {'url': 'https://tls.example:4318/'}
+EXTRA_KEYS: RECEIVER = {'foo': 'bar', 'baz': 42}
+
+
+@pytest.mark.parametrize("event",
+    [
+        "start",
+        "upgrade_charm",
+        "relation_changed",
+        "relation_broken",
+    ])
+@pytest.mark.parametrize("receiver",
+    [
+        {**GOOD_PROTOCOL, **GOOD_HTTP_URL},
+        {**GOOD_PROTOCOL, **GOOD_HTTPS_URL},
+    ])
+@pytest.mark.parametrize("extra_databag_fields", [{}])
+@pytest.mark.parametrize("extra_receiver", [None])
+@pytest.mark.parametrize("extra_receiver_fields", [{}])
+def test_foo(
+    sample_charm: Type[ops.CharmBase],
+    mock_destination: Mock,
+    ca_relation: ops.testing.Relation,
+    event: str,
+    receiver: RECEIVER,
+    extra_databag_fields: dict[str, str],
+    extra_receiver: RECEIVER | None,
+    extra_receiver_fields: RECEIVER,
+):
+    url = f'{receiver["url"].strip("/")}/v1/traces'  # type: ignore
+    ca = 'FIRST\nSECOND' if url.startswith('https') else None
+    charm_tracing_relation = ops.testing.Relation(
+        'charm-tracing',
+        id=0,
+        remote_app_data={'receivers': json.dumps([receiver])},
+    )
+
+    ctx = ops.testing.Context(sample_charm)
+    state = ops.testing.State(relations={charm_tracing_relation, ca_relation}, leader=True)
+
+    event_args = [charm_tracing_relation] if event.startswith('relation') else []
+    state = ctx.run(getattr(ctx.on, event)(*event_args), state)
+
+    if event == 'relation_broken':
+        mock_destination.assert_called_with(url=None, ca=None)
+    else:
+        assert state.get_relation(0).local_app_data == {'receivers': '["otlp_http"]'}
+        mock_destination.assert_called_with(url=url, ca=ca)
