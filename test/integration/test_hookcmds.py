9a4526ffb8cd3e5eae0c75f5148d36add2366704
Tony Meyer <tony.meyer@canonical.com>
2025-09-04 10:53:42 +1200

    Rename fixture, per review.

--- test/integration/test_hookcmds.py
+++ test/integration/test_hookcmds.py
@@ -21,44 +21,42 @@ import pytest
 
 
 @pytest.fixture
-def juju_post_deploy(build_relation_charm: Callable[[], str], juju: jubilant.Juju):
+def pack_and_deploy(build_relation_charm: Callable[[], str], juju: jubilant.Juju):
     """Fixture for testing hook commands."""
     charm_path = build_relation_charm()
     juju.deploy(charm_path)
     juju.wait(jubilant.all_active)
-    yield juju
+    yield
 
 
-def test_hookcmds_action(juju_post_deploy: jubilant.Juju):
-    task = juju_post_deploy.run(
-        'test-hookcmds/0', 'backup', params={'backup-location': 'loc', 'days': 7}
-    )
-    juju_post_deploy.wait(jubilant.all_active)
+def test_hookcmds_action(pack_and_deploy: None, juju: jubilant.Juju):
+    task = juju.run('test-hookcmds/0', 'backup', params={'backup-location': 'loc', 'days': 7})
+    juju.wait(jubilant.all_active)
     assert not task.success
     assert task.message == 'Oh no, I failed!'
     assert task.log == ['Asked to backup 7 days of data to loc']
     assert task.results == {'backup-location': 'loc', 'days+1': 8}
 
 
-def test_hookcmds_application_version(juju_post_deploy: jubilant.Juju):
-    status = juju_post_deploy.status()
+def test_hookcmds_application_version(pack_and_deploy: None, juju: jubilant.Juju):
+    status = juju.status()
     assert status.apps['test-hookcmds'].version == '1.25.79'
 
 
-def test_hookcmds_ports(juju_post_deploy: jubilant.Juju):
-    status = juju_post_deploy.status()
+def test_hookcmds_ports(pack_and_deploy: None, juju: jubilant.Juju):
+    status = juju.status()
     assert status.get_units('test-hookcmds')['test-hookcmds/0'].open_ports == []
-    juju_post_deploy.run('test-hookcmds/0', 'open-port', params={'protocol': 'tcp', 'port': 80})
-    status = juju_post_deploy.status()
+    juju.run('test-hookcmds/0', 'open-port', params={'protocol': 'tcp', 'port': 80})
+    status = juju.status()
     assert status.get_units('test-hookcmds')['test-hookcmds/0'].open_ports == ['80/tcp']
-    task = juju_post_deploy.run('test-hookcmds/0', 'opened-ports')
+    task = juju.run('test-hookcmds/0', 'opened-ports')
     assert task.results == {'opened_ports': ['80/tcp']}
-    juju_post_deploy.run('test-hookcmds/0', 'close-port', params={'protocol': 'tcp', 'port': 80})
-    status = juju_post_deploy.status()
+    juju.run('test-hookcmds/0', 'close-port', params={'protocol': 'tcp', 'port': 80})
+    status = juju.status()
     assert status.get_units('test-hookcmds')['test-hookcmds/0'].open_ports == []
 
 
-def test_hookcmds_config(juju_post_deploy: jubilant.Juju):
-    juju_post_deploy.config('test-hookcmds', {'log-level': 'DEBUG', 'another-option': 42})
-    log = juju_post_deploy.debug_log(limit=10)
+def test_hookcmds_config(pack_and_deploy: None, juju: jubilant.Juju):
+    juju.config('test-hookcmds', {'log-level': 'DEBUG', 'another-option': 42})
+    log = juju.debug_log(limit=10)
     assert "New config: {'log-level': 'DEBUG', 'another-option': 42'}" in log
