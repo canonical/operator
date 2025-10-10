8de04ca9deeeb65df15ce07562118a87fb7bf31c
James Garner <james.garner@canonical.com>
2025-10-10 15:10:05 +1300

    chore: satisfy type checker

--- test/test_real_pebble.py
+++ test/test_real_pebble.py
@@ -371,8 +371,8 @@ class TestPebbleStorageAPIsUsingRealPebble(PebbleStorageAPIsTestMixin):
     ):
         client.push(f'{pebble_dir}/test', os.urandom(1024 * 1024))  # chunk size is 16 * 2014
         tf = tempfile.NamedTemporaryFile(delete=False)  # noqa: SIM115
-        monkeypatch.setattr(pebble._FilesParser, 'get_response', lambda *args, **kwargs: None)
-        monkeypatch.setattr(tempfile, 'NamedTemporaryFile', lambda *args, **kwargs: tf)
+        monkeypatch.setattr(pebble._FilesParser, 'get_response', lambda self: None)  # type: ignore
+        monkeypatch.setattr(tempfile, 'NamedTemporaryFile', lambda *args, **kwargs: tf)  # type: ignore
         with pytest.raises(pebble.ProtocolError):
             client.pull(f'{pebble_dir}/test')
         assert not pathlib.Path(tf.name).exists()
