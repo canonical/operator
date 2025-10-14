32c45802cc9e80b72539b0533d5d95a0f82ca72e
Tony Meyer <tony.meyer@canonical.com>
2025-10-14 22:11:50 +1300

    Un-stringify per review.

--- ops/hookcmds/_types.py
+++ ops/hookcmds/_types.py
@@ -31,7 +31,7 @@ from ._utils import datetime_from_iso
 SecretRotate = Literal['never', 'hourly', 'daily', 'weekly', 'monthly', 'quarterly', 'yearly']
 SettableStatusName = Literal['active', 'blocked', 'maintenance', 'waiting']
 ReadOnlyStatusName = Literal['error', 'unknown']
-StatusName: TypeAlias = 'SettableStatusName | ReadOnlyStatusName'
+StatusName: TypeAlias = SettableStatusName | ReadOnlyStatusName
 
 
 class AddressDict(TypedDict):
