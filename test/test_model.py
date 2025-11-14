ce91f1ee91a09e5a7cb71de992fcc18124491794
Tony Meyer <tony.meyer@canonical.com>
2025-11-14 14:33:31 +1300

    Add test.

--- test/test_model.py
+++ test/test_model.py
@@ -3919,13 +3919,7 @@ class TestSecretClass:
         assert fake_script.calls(clear=True) == [
             ['secret-info-get', '--format=json', f'secret://{model._backend.model_uuid}/x'],
             ['secret-info-get', '--format=json', '--label', 'y'],
-            [
-                'secret-info-get',
-                '--format=json',
-                f'secret://{model._backend.model_uuid}/x',
-                '--label',
-                'y',
-            ],
+            ['secret-info-get', '--format=json', f'secret://{model._backend.model_uuid}/x'],
         ]
 
     def test_set_content(self, model: ops.Model, fake_script: FakeScript):
