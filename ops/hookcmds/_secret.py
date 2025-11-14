8b281083f8932174b291b9d84f43e46f33686925
Tony Meyer <tony.meyer@canonical.com>
2025-11-14 14:19:53 +1300

    fix: don't pass id and secret in secret-info-get.

--- ops/hookcmds/_secret.py
+++ ops/hookcmds/_secret.py
@@ -214,7 +214,7 @@ def secret_info_get(*, id: str | None = None, label: str | None = None) -> Secre
     args = ['--format=json']
     if id is not None:
         args.append(id)
-    if label is not None:
+    elif label is not None:  # elif because Juju secret-info-get doesn't allow id and label
         args.extend(['--label', label])
     stdout = run('secret-info-get', *args)
     result = cast('dict[str, Any]', json.loads(stdout))
