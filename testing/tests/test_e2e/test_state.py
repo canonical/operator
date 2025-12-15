8c1e771c05e8aae51bab8e332e40a0927566b67e
Tony Meyer <tony.meyer@canonical.com>
2025-12-15 14:36:27 +1300

    chore: remove unnecessary sorted().

--- testing/tests/test_e2e/test_state.py
+++ testing/tests/test_e2e/test_state.py
@@ -137,15 +137,13 @@ def test_status_setting(state, mycharm):
 
     # ignore stored state in the delta
     out_purged = replace(out, stored_states=state.stored_states)
-    expected = sorted(
-        [
-            {'op': 'replace', 'path': '/app_status/message', 'value': 'foo barz'},
-            {'op': 'replace', 'path': '/app_status/name', 'value': 'waiting'},
-            {'op': 'replace', 'path': '/unit_status/message', 'value': 'foo test'},
-            {'op': 'replace', 'path': '/unit_status/name', 'value': 'active'},
-        ],
-        key=lambda obj: obj['path'] + obj['op'],
-    )
+    # This should be sorted by path then op.
+    expected = [
+        {'op': 'replace', 'path': '/app_status/message', 'value': 'foo barz'},
+        {'op': 'replace', 'path': '/app_status/name', 'value': 'waiting'},
+        {'op': 'replace', 'path': '/unit_status/message', 'value': 'foo test'},
+        {'op': 'replace', 'path': '/unit_status/name', 'value': 'active'},
+    ]
     assert jsonpatch_delta(out_purged, state) == expected
 
 
