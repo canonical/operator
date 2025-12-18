4465156c398d202b16a14244935d2a0f0ca721ba
Tony Meyer <tony.meyer@canonical.com>
2025-12-19 08:49:52 +1300

    Update testing/tests/helpers.py
    Co-authored-by: James Garner <james.garner@canonical.com>

--- testing/tests/helpers.py
+++ testing/tests/helpers.py
@@ -73,5 +73,7 @@ def jsonpatch_delta(self: State, other: State) -> list[dict[str, Any]]:
     ):
         dict_other[attr] = [dataclasses.asdict(o) for o in dict_other[attr]]
         dict_self[attr] = [dataclasses.asdict(o) for o in dict_self[attr]]
+    # The jsonpatch library is untyped.
+    # See: https://github.com/stefankoegl/python-json-patch/issues/158
     patch = jsonpatch.make_patch(dict_other, dict_self).patch  # type: ignore
     return sorted(patch, key=lambda obj: obj['path'] + obj['op'])  # type: ignore
