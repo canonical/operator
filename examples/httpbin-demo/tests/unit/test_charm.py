6c346efe11c0ef7bb29ce6d97451a17d7b372726
David Wilding <david.wilding@canonical.com>
2025-12-23 12:33:08 +0800

    remove old config-changed test for deferred event

--- examples/httpbin-demo/tests/unit/test_charm.py
+++ examples/httpbin-demo/tests/unit/test_charm.py
@@ -123,24 +123,6 @@ def test_config_changed_valid_can_connect():
     assert state_out.unit_status == testing.ActiveStatus()
 
 
-def test_config_changed_valid_cannot_connect():
-    """Test a config-changed event when the config is valid but the container cannot be reached.
-
-    We expect to end up in MaintenanceStatus waiting for the deferred event to
-    be retried.
-    """
-    # Arrange:
-    ctx = testing.Context(HttpbinDemoCharm)
-    container = testing.Container(CONTAINER_NAME, can_connect=False)
-    state_in = testing.State(containers={container}, config={"log-level": "debug"})
-
-    # Act:
-    state_out = ctx.run(ctx.on.config_changed(), state_in)
-
-    # Assert:
-    assert isinstance(state_out.unit_status, testing.MaintenanceStatus)
-
-
 def test_config_changed_valid_uppercase():
     """Test a config-changed event when the config is valid and uppercase."""
     # Arrange:
