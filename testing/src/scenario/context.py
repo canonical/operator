bfd36f56209846576e1dcbdd6857e606414ae57e
James Garner <james.garner@canonical.com>
2026-03-19 14:25:03 +1300

    chore: restructure checks for actions and config to improve readability

--- testing/src/scenario/context.py
+++ testing/src/scenario/context.py
@@ -700,12 +700,18 @@ class Context(Generic[CharmType]):
 
         else:
             meta = copy.deepcopy(meta) if meta else {'name': str(charm_type.__name__)}
-            if actions is not None and 'actions' in meta:
+            if actions is None:
+                actions = meta.pop('actions', None)
+            elif 'actions' in meta:
                 raise ValueError('Cannot specify actions in both charmcraft.yaml and separately')
-            actions = copy.deepcopy(actions) if actions is not None else meta.pop('actions', None)
-            if config is not None and 'config' in meta:
+            else:
+                actions = copy.deepcopy(actions)
+            if config is None:
+                config = meta.pop('config', None)
+            elif 'config' in meta:
                 raise ValueError('Cannot specify config in both charmcraft.yaml and separately')
-            config = copy.deepcopy(config) if config is not None else meta.pop('config', None)
+            else:
+                config = copy.deepcopy(config)
             spec = _CharmSpec(charm_type=charm_type, meta=meta, actions=actions, config=config)
 
         self._charm_spec = spec
