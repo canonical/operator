a89b8e1054ab8bf97d122eba1c34f70d293a99f4
David Wilding <david.wilding@canonical.com>
2026-06-04 14:03:52 +0800

    add overwrite script and update conf.py

--- docs/custom_conf.py
+++ docs/custom_conf.py
@@ -93,7 +93,8 @@ copyright = '%s, %s' % (datetime.date.today().year, author)  # noqa: A001
 # don't know yet)
 # NOTE: If no ogp_* variable is defined (e.g. if you remove this section) the
 # sphinxext.opengraph extension will be disabled.
-ogp_site_url = 'https://documentation.ubuntu.com/ops/2.x/'
+version_slug = f'{os.environ.get("READTHEDOCS_VERSION", "local")}'
+ogp_site_url = f'https://canonical.com/juju/docs/ops/{version_slug}/'
 # The documentation website name (usually the same as the product name)
 ogp_site_name = project
 # The URL of an image or logo that is used in the preview
@@ -154,7 +155,7 @@ if os.environ.get('READTHEDOCS', '') == 'True':
 
 # If your project is on documentation.ubuntu.com, specify the project
 # slug (for example, "lxd") here.
-slug = 'ops'
+slug = 'juju/docs/ops'
 
 ############################################################
 ### Redirects
@@ -225,7 +226,9 @@ custom_excludes = [
 custom_html_css_files = []
 
 # Add JavaScript files (located in .sphinx/_static/)
-custom_html_js_files = []
+custom_html_js_files = [
+    'overwrite_links.js',
+]
 
 ## The following settings override the default configuration.
 
