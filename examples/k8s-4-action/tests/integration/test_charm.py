7d9dffa321b7d81f5a0bfe5d452e58ff1b0a3d5f
Trong Nhan Mai <trongnhan.mai@canonical.com>
2026-07-17 14:16:48 +1000

    fix: fix formatting issues

--- examples/k8s-4-action/tests/integration/test_charm.py
+++ examples/k8s-4-action/tests/integration/test_charm.py
@@ -48,7 +48,7 @@ def test_deploy(charm: pathlib.Path, juju: jubilant.Juju):
 
 def test_workload_version_is_set(charm: pathlib.Path, juju: jubilant.Juju):
     """Verify that the workload version has been set."""
-    juju.wait(lambda status: status.apps[APP_NAME].version == '1.0.4') # Hardcoded for simplicity.
+    juju.wait(lambda status: status.apps[APP_NAME].version == "1.0.4")  # Hardcoded for simplicity.
 
 
 @pytest.mark.juju_setup
